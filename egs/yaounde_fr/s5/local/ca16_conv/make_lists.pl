9c5b4b6afeb5a72e3b9059400ae38e2fdc2fa45f
John Josephmorgan Morgan <johnjosephmorgan@gmail.com>
2018-10-25 12:31:05 -0400

    Better names for conv data.

--- egs/yaounde_fr/s5/local/ca16_conv/make_lists.pl
+++ egs/yaounde_fr/s5/local/ca16_conv/make_lists.pl
@@ -5,11 +5,11 @@
 
 # make_lists.pl - write lists for acoustic model training
 # writes files under data/local/tmp/ca16conv/lists
+# This script associates a .wav file with a transcript.
 
 use strict;
 use warnings;
 use Carp;
-
 
 BEGIN {
     @ARGV == 1 or croak "USAGE: $0 <DATA_SRC_DIR>
@@ -25,8 +25,8 @@ use File::Basename;
 my ($d) = @ARGV;
 
 # initialize variables
-my $tmpdir = "data/local/tmp/ca16conv";
-my $p = "$d/transcripts/train/ca16_conv/transcripts.txt";
+my $tmpdir = "data/local/tmp/ca16conv_train";
+my $transcripts = "$d/transcripts/train/ca16_conv/transcripts.txt";
 # input wav file list
 my $w = "$tmpdir/wav_list.txt";
 # output temporary wav.scp file
@@ -35,14 +35,14 @@ my $o = "$tmpdir/lists/wav.scp";
 my $u = "$tmpdir/lists/utt2spk";
 # output temporary text files
 my $t = "$tmpdir/lists/text";
-# initialize hash for prompts
-my %p = ();
+# initialize hash for transcripts
+my %transcript = ();
 # done setting variables
 
 system "mkdir -p $tmpdir/lists";
-open my $P, '<', $p or croak "problem with $p $!";
+open my $TRANS, '<', $transcripts or croak "problem with $transcripts $!";
 # store prompts in hash
-LINEA: while ( my $line = <$P> ) {
+LINEA: while ( my $line = <$TRANS> ) {
   chomp $line;
   my ($j,$sent) = split /\s/, $line, 2;
   my ($volume,$directories,$file) = File::Spec->splitpath( $j );
@@ -52,9 +52,9 @@ LINEA: while ( my $line = <$P> ) {
   my $bn = 'gabonconv_' . $s . '_' . $i;
       # dashes?
   $sent =~ s/(\w)(\p{dash_punctuation}+?)/$1 $2/g;
-  $p{$bn} = $sent;
+  $transcript{$bn} = $sent;
 }
-close $P;
+close $TRANS;
 
 open my $W, '<', $w or croak "problem with $w $!";
 open my $O, '+>', $o or croak "problem with $o $!";
@@ -72,9 +72,9 @@ LINE: while ( my $line = <$W> ) {
   my $bn = 'gabonconv_' . $s . '_' . $i;
 
   # only work with utterances in transcript file
-  if ( exists $p{$bn} ) {
+  if ( exists $transcript{$bn} ) {
     my $fn = $bn . ".wav";
-    print $T "$bn $p{$bn}\n";
+    print $T "$bn $transcript{$bn}\n";
     print $O "$bn sox $line -t .wav - |\n";
     print $U "$bn gabonconv_${s}\n";
   } else {
--- egs/yaounde_fr/s5/local/prepare_data.sh
+++ egs/yaounde_fr/s5/local/prepare_data.sh
@@ -20,13 +20,12 @@ local/ca16_conv/prepare_data.sh $datadir
 local/ca16_read_devtest/prepare_data.sh $datadir
 local/ca16_read_train/prepare_data.sh $datadir
 local/ca16_test/prepare_data.sh $datadir
-exit
 local/niger_dev/prepare_data.sh $datadir
 local/yaounde/prepare_data.sh $datadir
 
 echo "$0: Consolidating training data lists"
 mkdir -p data/train
-for c in  ca16conv ca16read_train yaounde; do
+for c in  ca16conv_train ca16read_train yaounde; do
   for x in wav.scp text utt2spk; do
     cat data/local/tmp/$c/lists/$x | tr "	" " " >> data/train/$x
   done
