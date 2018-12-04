2011e720db3c6423ccfba9222cadaf4df799afd5
John Josephmorgan Morgan <johnjosephmorgan@gmail.com>
2018-12-04 14:55:03 -0500

    Update.

--- egs/wsj/s5/utils/parallel/pbspro.pl
+++ egs/wsj/s5/utils/parallel/pbspro.pl
@@ -164,8 +164,9 @@ if ($array_job == 0) { # not an array job
 }
 print $Q "exit \$[\$ret ? 1 : 0]\n"; # avoid status 100 which grid-engine
 print $Q "## submitted with:\n";
-croak "hello";
+
 my $qsub_cmd .= "-o $queue_logfile $qsub_opts $queue_array_opt $queue_scriptfile >>$queue_logfile 2>&1";
+croak "hello\t$qsub_cmd";
 
 print $Q "# $qsub_cmd\n";
 if (!close $Q) { # close was not successful... || croak "Could not close script file $shfile $!";
