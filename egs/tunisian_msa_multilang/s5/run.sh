e8f025939dcfd644ca6631f9eb64814df1bb5845
John Josephmorgan Morgan <johnjosephmorgan@gmail.com>
2018-12-12 18:40:24 -0500

    Pumping up the frames per chunk.

--- egs/tunisian_msa_multilang/s5/run.sh
+++ egs/tunisian_msa_multilang/s5/run.sh
@@ -416,7 +416,7 @@ if [ $stage -le 44 ]; then
   for fld in ${decode_tamsa_folds[@]}; do
     nspk=$(wc -l <data/tamsa/$fld/spk2utt)
     steps/nnet3/decode.sh --nj $nspk \
-      --iter final_adj --stage -1 --beam 20.0 --lattice-beam 8.5 \
+      --iter final_adj --stage -1 --beam 16.0 --lattice-beam 8.5 \
       exp/tamsa/tri3b/graph \
         data/tamsa/$fld $dir/tamsa/decode_${fld}
   done
@@ -427,6 +427,7 @@ if [ $stage -le 45 ]; then
   for fld in ${decode_mini_librispeech_folds[@]}; do
     nspk=$(wc -l <data/mini_librispeech/$fld/spk2utt)
     steps/nnet3/decode.sh --nj $nspk \
+      --frames-per-chunk 250 \
       --iter final_adj --stage -1 --beam 16.0 --lattice-beam 8.5 \
       exp/mini_librispeech/tri3b/graph \
       data/mini_librispeech/$fld $dir/mini_librispeech/decode_${fld}
