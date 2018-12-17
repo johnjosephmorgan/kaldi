2f221324a8103236969dbb5f4fc2b6e075c266bd
John Josephmorgan Morgan <johnjosephmorgan@gmail.com>
2018-12-17 13:34:58 -0500

    No name for coproc.

--- egs/tunisian_msa_multilang/s5/run.sh
+++ egs/tunisian_msa_multilang/s5/run.sh
@@ -64,11 +64,11 @@ fi
 # preparation stages will store files under data/
 # Delete the entire data directory when restarting.
 if [ $stage -le 2 ]; then
-  coproc tamsa_build local/tamsa/build_acoustic_models.sh
+  coproc local/tamsa/build_acoustic_models.sh
 fi
 
 if [ $stage -le 3 ]; then
-  coproc mini_librispeech_build local/mini_librispeech/build_acoustic_models.sh
+  coproc local/mini_librispeech/build_acoustic_models.sh
 fi
 
 wait
