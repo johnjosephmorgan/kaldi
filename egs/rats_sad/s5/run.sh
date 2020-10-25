efe1180e789add349e1f8c189126e44844f585f8
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-10-25 12:58:44 -0400

    Fixing folds.

--- egs/rats_sad/s5/run.sh
+++ egs/rats_sad/s5/run.sh
@@ -26,34 +26,51 @@ rats_sad_data_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data
 rats_sad_dev_1_tab_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data/dev-1/sad
 rats_sad_dev_2_tab_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data/dev-2/sad
 rats_sad_train_tab_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data/train/sad
+rats_sad_dev_1_audio_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data/dev-1/audio
+rats_sad_dev_2_audio_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data/dev-2/audio
+rats_sad_train_audio_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data/train/audio
 
 if [ $stage -le 0 ]; then
-    echo "$0 Stage 0: Get  all info files."
-    mkdir -p data/local/annotations
-  find $rats_sad_data_dir/train/sad -type f -name "*.tab" | xargs cat > data/local/annotations/train.txt
-  find $rats_sad_data_dir/dev-1 -type f -name "*.tab" | xargs cat > data/local/annotations/dev.txt
-  find $rats_sad_data_dir/dev-2 -type f -name "*.tab" | xargs cat > data/local/annotations/eval.txt
+  echo "$0 Stage 0: Get  all info files."
+  mkdir -p data/local/annotations
+  find $rats_sad_train_tab_dir -type f -name "*.tab" | xargs cat > data/local/annotations/train.txt
+  find $rats_sad_dev_1_tab_dir -type f -name "*.tab" | xargs cat > data/local/annotations/dev.txt
+  find $rats_sad_dev_2_tab_dir -type f -name "*.tab" | xargs cat > data/local/annotations/eval.txt
 fi
 
 if [ $stage -le 1 ]; then
   echo "$0 Stage 1: Preparing data directories."
-  for dataset in train dev eval; do
-    echo "$0: preparing $dataset set."
-    mkdir -p data/$dataset
-    local/prepare_data.py data/local/annotations/${dataset}.txt \
-      $rats_sad_data_dir/$dataset/audio data/$dataset
-    local/convert_rttm_to_utt2spk_and_segments.py --append-reco-id-to-spkr=true data/$dataset/rttm.annotation \
-      <(awk '{print $2" "$2" "$3}' data/$dataset/rttm.annotation |sort -u) \
-      data/$dataset/utt2spk data/$dataset/segments
+  echo "$0: preparing training set."
+  mkdir -p data/train
+  local/prepare_data.py data/local/annotations/train.txt \
+    $rats_sad_train_audio_dir data/train
+
+  echo "$0: preparing dev set."
+  mkdir -p data/dev
+  local/prepare_data.py data/local/annotations/dev.txt \
+    $rats_sad_dev_audio_dir data/dev
+
+  echo "$0: preparing eval set."
+  mkdir -p data/eval
+  local/prepare_data.py data/local/annotations/eval.txt \
+    $rats_sad_eval_audio_dir data/eval
+fi
+
+if [ $stage -le 2 ]; then
+  echo "$0 Stage 2: Convert rttm files to utt2spk."
+  for fld in train dev eval; do
+    local/convert_rttm_to_utt2spk_and_segments.py --append-reco-id-to-spkr=true data/$fld/rttm.annotation \
+      <(awk '{print $2" "$2" "$3}' data/$fld/rttm.annotation |sort -u) \
+      data/$fld/utt2spk data/$fld/segments
 
     # For the test sets we create dummy segments and utt2spk files using oracle speech marks
-    if ! [ $dataset == "train" ]; then
-      local/get_all_segments.py data/$dataset/rttm.annotation > data/$dataset/segments
-      awk '{print $1,$2}' data/$dataset/segments > data/$dataset/utt2spk
+    if ! [ $fld == "train" ]; then
+      local/get_all_segments.py data/$fld/rttm.annotation > data/$fld/segments
+      awk '{print $1,$2}' data/$fld/segments > data/$fld/utt2spk
     fi
 
-    utils/utt2spk_to_spk2utt.pl data/$dataset/utt2spk > data/$dataset/spk2utt
-    utils/fix_data_dir.sh data/$dataset
+    utils/utt2spk_to_spk2utt.pl data/$fld/utt2spk > data/$fld/spk2utt
+    utils/fix_data_dir.sh data/$fld
   done
 fi
 
