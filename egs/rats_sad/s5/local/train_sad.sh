310070d71bc0a2c778fd6b003560b642138f4053
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-10-26 12:36:28 -0400

    Fixing arguments to training script.

--- egs/rats_sad/s5/local/train_sad.sh
+++ egs/rats_sad/s5/local/train_sad.sh
@@ -47,7 +47,7 @@ fi
 if [ $stage -le 1 ]; then
   echo "$0 Stage 1: Get segments."
   #local/get/_speech_activity_segments.py $ref_rttm > $whole_data_dir/sad.rttm
-  local/segmentation/get_sad_targets.py --reco2num-frames data/train_whole/utt2dur \
+  local/segmentation/get_sad_targets.py data/train_whole/utt2dur \
     $ref_rttm > $whole_data_dir/sad.rttm
 fi
 
