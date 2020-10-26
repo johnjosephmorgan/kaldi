c9b8a7b20e1e657bf0f4eb003d2ef835bd561bf8
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-10-26 17:40:07 -0400

    Simplified script.

--- egs/rats_sad/s5/local/rats_sad_text_prep.sh
+++ egs/rats_sad/s5/local/rats_sad_text_prep.sh
@@ -2,53 +2,23 @@
 
 # Copyright 2020 ARL (Author: John Morgan)
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   echo "Usage: $0 <rats_sad_dir> <output_dir>"
-  echo "<rats_sad_dir> Source Rats corpus location
-  echo " <output-dir>: output location
+  echo "$0 <rats_sad_dir>"
+  echo "<rats_sad_dir>: Source data location"
   echo "For example:"
-  echo "$0 john@$GPUTHREE:/mnt/corpora/LDC2015S02/RATS_SAD/data data/local/downloads"
+  echo "$0 /mnt/corpora/LDC2015S02/RATS_SAD/data data/local/downloads"
   exit 1;
 fi
 
 set -eux
-src_dir=$1
-out_dir=$2
+dir=$1
+dev_1_dir=$dir/dev-1/sad
+dev_2_dir=$dir/dev-2/sad
+train_dir=$dir/train/sad
 
-mkdir -p $out_dir
+mkdir -p data/local/annotations
 
-echo "$0: Copying annotations."
-for fld in dev-1 dev-2 train; do
-  cp -Rv ${src_dir}/$fld/sad $out_dir
-done
-
-exit
-annotver=ami_public_manual_1.6.1
-annot="$dir/$annotver"
-
-logdir=data/local/downloads; mkdir -p $logdir/log
-[ ! -f $annot.zip ] && wget -nv -O $annot.zip $amiurl/AMICorpusAnnotations/$annotver.zip &> $logdir/log/download_ami_annot.log
-
-if [ ! -d $dir/annotations ]; then
-  mkdir -p $dir/annotations
-  unzip -o -d $dir/annotations $annot.zip &> /dev/null
-fi
-
-[ ! -f "$dir/annotations/AMI-metadata.xml" ] && echo "$0: File AMI-Metadata.xml not found under $dir/annotations." && exit 1;
-
-
-# extract text from AMI XML annotations,
-local/ami_xml2text.sh $dir
-
-wdir=data/local/annotations
-[ ! -f $wdir/transcripts1 ] && echo "$0: File $wdir/transcripts1 not found." && exit 1;
-
-echo "Preprocessing transcripts..."
-local/ami_split_segments.pl $wdir/transcripts1 $wdir/transcripts2 &> $wdir/log/split_segments.log
-
-# make final train/dev/eval splits
-for dset in train eval dev; do
-  grep -f local/split_$dset.orig $wdir/transcripts2 > $wdir/$dset.txt
-done
-
-
+find $train_dir -type f -name "*.tab" | xargs cat > data/local/annotations/train.txt
+find $dev_1_dir -type f -name "*.tab" | xargs cat > data/local/annotations/dev.txt
+find $dev_2_dir -type f -name "*.tab" | xargs cat > data/local/annotations/eval.txt
