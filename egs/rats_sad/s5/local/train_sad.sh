bb799e2db6c5d05e327b339e084a4041d5610f07
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-10-26 13:02:42 -0400

    Minor fix.

--- egs/rats_sad/s5/local/train_sad.sh
+++ egs/rats_sad/s5/local/train_sad.sh
@@ -48,7 +48,7 @@ if [ $stage -le 1 ]; then
   echo "$0 Stage 1: Get targets."
   local/segmentation/get_sad_targets.py \
     data/train_whole/utt2num_frames \
-    data/train_whole/sad.rttm $dir/targets.ark \
+    data/train_whole/sad.rttm $dir/targets.ark
 fi
 
 if [ $stage -le 2 ]; then
