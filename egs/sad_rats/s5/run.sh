1a067b74cef6f7eb491ccf2ee13b59b8b2d4d75a
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-04 16:01:46 -0500

    Removed exit.

--- egs/sad_rats/s5/run.sh
+++ egs/sad_rats/s5/run.sh
@@ -21,7 +21,7 @@ if [ $stage -le 0 ]; then
   echo "$0 Stage 0: Get  all info files."
   local/rats_sad_text_prep.sh $rats_sad_data_dir
 fi
-exit
+
 if [ $stage -le 1 ]; then
   echo "$0 Stage 1: Preparing data directories."
   for fld in train $test_sets ; do
