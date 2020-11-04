a0dfc1013a761042f92ab58c47bc61b3a21b6ccc
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-04 13:38:59 -0500

    Initial commit for sad rats.

---
+++ egs/sad_rats/s5/run.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+# Copyright   2020   ARL (Author: John Morgan)
+# Apache 2.0.
+
+# This recipe builds a Speech Activity Detection system on the rats_sad corpus.
+# The LDC identifyer for the rats_sad corpus is LDC2015S02.
+
+. ./cmd.sh
+. ./path.sh
+set -euo pipefail
+stage=0
+sad_stage=0
+# Path where RATS_SAD gets downloaded (or where locally available):
+rats_sad_data_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data
+nj=16
+decode_nj=8
+train_set=train
+test_sets="dev-1 dev-2 "
+
+. utils/parse_options.sh
+
+if [ $stage -le 0 ]; then
+  echo "$0 Stage 0: Get  all info files."
+  local/rats_sad_text_prep.sh $rats_sad_data_dir
+fi
+
+if [ $stage -le 1 ]; then
+  echo "$0 Stage 1: Preparing data directories."
+  for fld in train $test_sets ; do
+    echo "$0: preparing $fld set."
+    mkdir -p data/$fld
+    local/prepare_data.py data/local/annotations/$fld.txt \
+      $rats_sad_data_dir/$fld/audio/ data/$fld
+  done
+fi
+
+if [ $stage -le 2 ]; then
+  for fld in train $test_sets ; do
+    echo "$0 Stage 2: Convert $fld rttm files to utt2spk."
+    local/convert_rttm_to_utt2spk_and_segments.py --use-reco-id-as-spkr=true \
+      data/$fld/rttm.annotation \
+      <(awk '{print $2" "$2" "$3}' data/$fld/rttm.annotation |sort -u) \
+      data/$fld/utt2spk data/$fld/segments
+
+    utils/utt2spk_to_spk2utt.pl data/$fld/utt2spk > data/$fld/spk2utt
+    utils/fix_data_dir.sh data/$fld
+  done
+fi
+
+if [ $stage -le 3 ]; then
+  echo "$0 Stage 3: training a Speech Activity detector."
+  local/train_sad.sh --stage $sad_stage --test-sets "$test_sets"
+fi
+
+sad_affix=1a
+if [ $stage -le 4 ]; then
+  for dataset in $test_sets; do
+    echo "$0 Stage 4: Run SAD detection."
+    local/detect_overlaps.sh \
+      --convert_data_dir_to_whole true \
+      --output-scale "1 2 1" \
+      data/${dataset} \
+      exp/segmentation_${sad_affix}/tdnn_lstm_asr_sad_${sad_affix} \
+      exp/segmentation_${sad_affix}/sad_${dataset}
+  done
+fi
+
+if [ $stage -le 5 ]; then
+  for fld in $test_sets; do
+    echo "$0 Stage 5: evaluating $fld output."
+  steps/overlap/get_overlap_segments.py data/$fld/rttm.annotation | \
+      md-eval.pl -r - -s exp/segmentation_${sad_affix}/sad_${fld}/rttm_overlap |\
+      awk 'or(/MISSED SPEAKER TIME/,/FALARM SPEAKER TIME/)'
+  done
+fi
