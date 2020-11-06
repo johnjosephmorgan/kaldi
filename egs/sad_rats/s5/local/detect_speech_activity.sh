899f4bad34b5069f5ebbcd5c29e98358bf110e54
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-06 09:01:53 -0500

    Modified detection script.

--- egs/sad_rats/s5/local/detect_speech_activity.sh
+++ egs/sad_rats/s5/local/detect_speech_activity.sh
@@ -2,12 +2,10 @@
 
 # Copyright 2016-17  Vimal Manohar
 #              2017  Nagendra Kumar Goel
+# Copyright  2020 John Morgan
 # Apache 2.0.
 
-# This script does nnet3-based speech activity detection given an input 
-# kaldi data directory and outputs a segmented kaldi data directory.
-# This script can also do music detection and other similar segmentation
-# using appropriate options such as --output-name output-music.
+# This script does nnet3-based speech activity detection.
 
 set -e 
 set -o pipefail
@@ -15,36 +13,23 @@ set -u
 
 if [ -f ./path.sh ]; then . ./path.sh; fi
 
-affix=  # Affix for the segmentation
-nj=32
+affix=1a  # Affix for the segmentation
+nj=10
 cmd=run.pl
 stage=-1
 
 # Feature options (Must match training)
 mfcc_config=conf/mfcc_hires.conf
-feat_affix=   # Affix for the type of feature used
-
-convert_data_dir_to_whole=true    # If true, the input data directory is 
-                                  # first converted to whole data directory (i.e. whole recordings)
-                                  # and segmentation is done on that.
-                                  # If false, then the original segments are 
-                                  # retained and they are split into sub-segments.
-
 output_name=output   # The output node in the network
-sad_name=sad    # Base name for the directory storing the computed loglikes
-                # Can be music for music detection
-segmentation_name=segmentation  # Base name for the directory doing segmentation
-                                # Can be segmentation_music for music detection
 
 # SAD network config
 iter=final  # Model iteration to use
 
-# Contexts must ideally match training for LSTM models, but
-# may not necessarily for stats components
-extra_left_context=0  # Set to some large value, typically 40 for LSTM (must match training)
-extra_right_context=0  
-extra_left_context_initial=-1
-extra_right_context_final=-1
+# Contexts must ideally match training for LSTM models
+extra_left_context=60  # Set to some large value, typically 40 for LSTM (must match training)
+extra_right_context=10  
+extra_left_context_initial=0
+extra_right_context_final=0
 frames_per_chunk=150
 
 # Decoding options
@@ -58,111 +43,61 @@ transform_probs_opts=""
 
 # Postprocessing options
 segment_padding=0.2   # Duration (in seconds) of padding added to segments 
-min_segment_dur=0   # Minimum duration (in seconds) required for a segment to be included
-                    # This is before any padding. Segments shorter than this duration will be removed.
-                    # This is an alternative to --min-speech-duration above.
-merge_consecutive_max_dur=0   # Merge consecutive segments as long as the merged segment is no longer than this many
-                              # seconds. The segments are only merged if their boundaries are touching.
-                              # This is after padding by --segment-padding seconds.
-                              # 0 means do not merge. Use 'inf' to not limit the duration.
+# min_segment_dur:
+# Minimum duration (in seconds) required for a segment to be included
+# This is before any padding. Segments shorter than this duration will be removed.
+# This is an alternative to --min-speech-duration above.
+min_segment_dur=0
+# merge_consecutive_max_dur:
+#Merge consecutive segments
+# as long as the merged segment is no longer than this many seconds.
+#The segments are only merged if their boundaries are touching.
+# This is after padding by --segment-padding seconds.
+# 0 means do not merge. Use 'inf' to not limit the duration.
+merge_consecutive_max_dur=inf
 
-echo $* 
+echo "$0 $*"
 
 . utils/parse_options.sh
 
-if [ $# -ne 5 ]; then
-  echo "This script does nnet3-based speech activity detection given an input kaldi "
-  echo "data directory and outputs an output kaldi data directory."
-  echo "See script for details of the options to be supplied."
-  echo "Usage: $0 <src-data-dir> <sad-nnet-dir> <mfcc-dir> <work-dir> <out-data-dir>"
-  echo " e.g.: $0 ~/workspace/egs/ami/s5b/data/sdm1/dev exp/nnet3_sad_snr/nnet_tdnn_j_n4 \\"
-  echo "    mfcc_hires exp/segmentation_sad_snr/nnet_tdnn_j_n4 data/ami_sdm1_dev"
+if [ $# -ne 1 ]; then
+  echo "This script does nnet3-based speech activity detection."
+  "Input is a kaldi  directory."
+  echo "Outputs is also a kaldi data directory."
+  echo "Usage: $0 <src-data-dir>
+  echo "<src_data_dir>: The input data directory that needs to be segmented.
+  echo "For example :"
+  $0 data/dev-1
   echo ""
-  echo "Options: "
-  echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
-  echo "  --nj <num-job>                                 # number of parallel jobs to run."
-  echo "  --stage <stage>                                # stage to do partial re-run from."
-  echo "  --convert-data-dir-to-whole <true|false>    # If true, the input data directory is "
-  echo "                                              # first converted to whole data directory (i.e. whole recordings) "
-  echo "                                              # and segmentation is done on that."
-  echo "                                              # If false, then the original segments are "
-  echo "                                              # retained and they are split into sub-segments."
-  echo "  --output-name <name>    # The output node in the network"
-  echo "  --extra-left-context  <context|0>   # Set to some large value, typically 40 for LSTM (must match training)"
-  echo "  --extra-right-context  <context|0>   # For BLSTM or statistics pooling"
   exit 1
 fi
 
-src_data_dir=$1   # The input data directory that needs to be segmented.
-                  # If convert_data_dir_to_whole is true, any segments in that will be ignored.
-sad_nnet_dir=$2   # The SAD neural network
-mfcc_dir=$3       # The directory to store the features
-dir=$4            # Work directory
-data_dir=$5       # The output data directory will be ${data_dir}_seg
+fld=$1
 
-affix=${affix:+_$affix}
-feat_affix=${feat_affix:+_$feat_affix}
+affix=1a
+dir=exp/segmentation_${affix}/tdnn_lstm_asr_sad_${afix}
 
-data_id=`basename $data_dir`
-sad_dir=${dir}/${sad_name}${affix}_${data_id}_whole${feat_affix}
-seg_dir=${dir}/${segmentation_name}${affix}_${data_id}_whole${feat_affix}
-
-if $convert_data_dir_to_whole; then
-  test_data_dir=data/${data_id}_whole${feat_affix}_hires
-  if [ $stage -le 0 ]; then
-    rm -r ${test_data_dir} || true
-    utils/data/convert_data_dir_to_whole.sh $src_data_dir ${test_data_dir}
-  fi
-else
-  test_data_dir=data/${data_id}${feat_affix}_hires
-  if [ $stage -le 0 ]; then
-    rm -r ${test_data_dir} || true
-    utils/copy_data_dir.sh $src_data_dir $test_data_dir
-  fi
+if [ $stage -le 0 ]; then
+    utils/data/convert_data_dir_to_whole.sh data/$fld data/${fld}_whole
 fi
-
-###############################################################################
-## Extract input features 
-###############################################################################
 
 if [ $stage -le 1 ]; then
-  utils/fix_data_dir.sh $test_data_dir
+  echo "$0: Extract input features in data/${fld}_whole."
+  utils/fix_data_dir.sh data/${fld}_whole
   steps/make_mfcc.sh --mfcc-config $mfcc_config --nj $nj --cmd "$cmd" --write-utt2num-frames true \
-    ${test_data_dir} exp/make_hires$feat_affix/${data_id} $mfcc_dir
-  steps/compute_cmvn_stats.sh ${test_data_dir} exp/make_hires$feat_affix/${data_id} $mfcc_dir
-  utils/fix_data_dir.sh ${test_data_dir}
+    data/${fld}_whole 
+  steps/compute_cmvn_stats.sh data/${fld}_whole
+  utils/fix_data_dir.sh data/${fld}_whole
 fi
 
-###############################################################################
-## Forward pass through the network network and dump the log-likelihoods.
-###############################################################################
-
 frame_subsampling_factor=1
-if [ -f $sad_nnet_dir/frame_subsampling_factor ]; then
-  frame_subsampling_factor=$(cat $sad_nnet_dir/frame_subsampling_factor)
+if [ -f $dir/frame_subsampling_factor ]; then
+  frame_subsampling_factor=$(cat $dir/frame_subsampling_factor)
 fi
 
 mkdir -p $dir
-if [ $stage -le 4 ]; then
-  if [ "$(readlink -f $sad_nnet_dir)" != "$(readlink -f $dir)" ]; then
-    cp $sad_nnet_dir/cmvn_opts $dir || exit 1
-  fi
-
-  ########################################################################
-  ## Initialize neural network for decoding using the output $output_name
-  ########################################################################
-
-  if [ ! -z "$output_name" ] && [ "$output_name" != output ]; then
-    $cmd $dir/log/get_nnet_${output_name}.log \
-      nnet3-copy --edits="rename-node old-name=$output_name new-name=output" \
-      $sad_nnet_dir/$iter.raw $dir/${iter}_${output_name}.raw || exit 1
-    iter=${iter}_${output_name}
-  else 
-    if ! diff $sad_nnet_dir/$iter.raw $dir/$iter.raw; then
-      cp $sad_nnet_dir/$iter.raw $dir/
-    fi
-  fi
-
+if [ $stage -le 2 ]; then
+  echo "$0 Stage 2: Forward pass through the network network and dump the log-likelihoods."
   steps/nnet3/compute_output.sh --nj $nj --cmd "$cmd" \
     --iter ${iter} \
     --extra-left-context $extra_left_context \
@@ -171,20 +106,16 @@ if [ $stage -le 4 ]; then
     --extra-right-context-final $extra_right_context_final \
     --frames-per-chunk $frames_per_chunk --apply-exp true \
     --frame-subsampling-factor $frame_subsampling_factor \
-    ${test_data_dir} $dir $sad_dir || exit 1
+    data/${fld}_whole $dir $dir || exit 1
 fi
 
-###############################################################################
-## Prepare FST we search to make speech/silence decisions.
-###############################################################################
-
-utils/data/get_utt2dur.sh --nj $nj --cmd "$cmd" $test_data_dir || exit 1
-frame_shift=$(utils/data/get_frame_shift.sh $test_data_dir) || exit 1
-
+utils/data/get_utt2dur.sh --nj $nj --cmd "$cmd" data/${fld{_whole || exit 1
+frame_shift=$(utils/data/get_frame_shift.sh data/${fld}_whole) || exit 1
 graph_dir=${dir}/graph_${output_name}
-if [ $stage -le 5 ]; then
-  mkdir -p $graph_dir
 
+if [ $stage -le 3 ]; then
+  echo "$0 Stage 3:  Prepare FST to make speech/silence decisions."
+  mkdir -p $graph_dir
   # 1 for silence and 2 for speech
   cat <<EOF > $graph_dir/words.txt
 <eps> 0
@@ -199,53 +130,45 @@ EOF
       $graph_dir/HCLG.fst
 fi
 
-###############################################################################
-## Do Viterbi decoding to create per-frame alignments.
-###############################################################################
-
-post_vec=$sad_nnet_dir/post_${output_name}.vec
-if [ ! -f $sad_nnet_dir/post_${output_name}.vec ]; then
-  if [ ! -f $sad_nnet_dir/post_${output_name}.txt ]; then
-    echo "$0: Could not find $sad_nnet_dir/post_${output_name}.vec. "
+post_vec=$dir/post_${output_name}.vec
+if [ ! -f $dir/post_${output_name}.vec ]; then
+  if [ ! -f $dir/post_${output_name}.txt ]; then
+    echo "$0: Could not find $dir/post_${output_name}.vec. "
     echo "Re-run the corresponding stage in the training script possibly "
     echo "with --compute-average-posteriors=true or compute the priors "
     echo "from the training labels"
     exit 1
   else
-    post_vec=$sad_nnet_dir/post_${output_name}.txt
+    post_vec=$dir/post_${output_name}.txt
   fi
 fi
 
-mkdir -p $seg_dir
-if [ $stage -le 6 ]; then
+if [ $stage -le 4 ]; then
   steps/segmentation/internal/get_transform_probs_mat.py \
-    --priors="$post_vec" $transform_probs_opts > $seg_dir/transform_probs.mat
+    --priors="$post_vec" $transform_probs_opts > $dir/transform_probs.mat
 
   steps/segmentation/decode_sad.sh --acwt $acwt --cmd "$cmd" \
     --nj $nj \
-    --transform "$seg_dir/transform_probs.mat" \
-    $graph_dir $sad_dir $seg_dir
+    --transform "$dir/transform_probs.mat" \
+    $graph_dir $dir $dir
 fi
 
-###############################################################################
-## Post-process segmentation to create kaldi data directory.
-###############################################################################
-
-if [ $stage -le 7 ]; then
+if [ $stage -le 5 ]; then
+    echo "$0 Stage 5: Post-process segmentation to create kaldi data directory."
   steps/segmentation/post_process_sad_to_segments.sh \
     --segment-padding $segment_padding --min-segment-dur $min_segment_dur \
     --merge-consecutive-max-dur $merge_consecutive_max_dur \
     --cmd "$cmd" --frame-shift $(perl -e "print $frame_subsampling_factor * $frame_shift") \
-    ${test_data_dir} ${seg_dir} ${seg_dir}
+    data/${fld}_whole ${dir} ${dir}
 fi
 
-if [ $stage -le 8 ]; then
-  utils/data/subsegment_data_dir.sh ${test_data_dir} ${seg_dir}/segments \
-    ${data_dir}_seg
-  cp $src_data_dir/wav.scp ${data_dir}_seg
-  cp $src_data_dir/{stm,reco2file_and_channel,glm} ${data_dir}_seg/ || true
-  utils/fix_data_dir.sh ${data_dir}_seg
+if [ $stage -le 6 ]; then
+  utils/data/subsegment_data_dir.sh daa/${fld}_whole $dir/segments \
+    data/${fld}_seg
+  cp data/$fld}/wav.scp data/${fld}_seg
+  cp data/${fld}/{stm,reco2file_and_channel,glm} data/${fld}_seg/ || true
+  utils/fix_data_dir.sh data/${fld}_seg
 fi
 
-echo "$0: Created output segmented kaldi data directory in ${data_dir}_seg"
+echo "$0: Created output segmented kaldi data directory in data/${fld}_seg"
 exit 0
