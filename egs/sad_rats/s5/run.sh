3ac9360a68c6bd4d0b425042a23973ef817db6b1
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-07 16:23:45 -0500

    Use exp/sad instead of exp/segmentation.

--- egs/sad_rats/s5/run.sh
+++ egs/sad_rats/s5/run.sh
@@ -14,7 +14,7 @@ rats_sad_data_dir=/mnt/corpora/LDC2015S02/RATS_SAD/data
 nj=10
 test_sets="dev-1 dev-2 "
 affix=1a
-dir=exp/segmentation_${affix}
+dir=exp/sad_${affix}
 
 . utils/parse_options.sh
 
