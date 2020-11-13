ac19708ab5b68c74b76cb74387c8be7dfb5db2fa
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-13 09:26:14 -0500

    Set rttm filename.

--- egs/rats_sad/s5/run.sh
+++ egs/rats_sad/s5/run.sh
@@ -104,7 +104,7 @@ if [ $stage -le 9 ]; then
   for fld in $test_sets; do
     echo "$0 Stage 9: evaluating on $fld output."
     steps/overlap/get_overlap_segments.py data/$fld/rttm.annotation | \
-      md-eval.pl -r - -s $dir/$fld/overlap.rttm >\
+      md-eval.pl -r - -s $dir/$fld/rttm_overlap >\
       $dir/$fld/results.txt
   done
 fi
