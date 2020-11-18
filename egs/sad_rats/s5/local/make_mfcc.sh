19db1c7b362fc0e625c0d24382ddcb0b78469138
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-18 12:26:01 -0500

    Allow upsampling.

--- egs/sad_rats/s5/local/make_mfcc.sh
+++ egs/sad_rats/s5/local/make_mfcc.sh
@@ -118,7 +118,7 @@ if [ -f $data/segments ]; then
   $cmd JOB=1:$nj $logdir/make_mfcc_${name}.JOB.log \
     extract-segments scp,p:$scp $logdir/segments.JOB ark:- \| \
     compute-mfcc-feats $vtln_opts $write_utt2dur_opt --verbose=2 \
-      --config=$mfcc_config ark:- ark:- \| \
+      --config=$mfcc_config --allow-upsample=true ark:- ark:- \| \
     copy-feats --compress=$compress $write_num_frames_opt ark:- \
       ark,scp:$mfccdir/raw_mfcc_$name.JOB.ark,$mfccdir/raw_mfcc_$name.JOB.scp \
      || exit 1;
