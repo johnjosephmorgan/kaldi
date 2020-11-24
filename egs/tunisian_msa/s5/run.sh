bc870fc223dfbc36c818f9ba627026a8e9dcc09f
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-24 12:38:28 -0500

    No testing until SAT.

--- egs/tunisian_msa/s5/run.sh
+++ egs/tunisian_msa/s5/run.sh
@@ -28,50 +28,54 @@ subs_src="http://opus.nlpl.eu/download.php?f=OpenSubtitles2018/mono/OpenSubtitle
 if [ $stage -le 1 ]; then
   # Downloads archive to this script's directory
   local/tamsa_download.sh $speech
+fi
 
+if [ $stage -le 2 ]; then
   local/qcri_lexicon_download.sh $lex
+fi
 
+if [ $stage -le 3 ]; then
   local/subs_download.sh $subs_src
 fi
 
 # preparation stages will store files under data/
 # Delete the entire data directory when restarting.
-if [ $stage -le 2 ]; then
+if [ $stage -le 4 ]; then
   local/prepare_data.sh
 fi
 
-if [ $stage -le 3 ]; then
+if [ $stage -le 5 ]; then
   mkdir -p $tmpdir/dict
   local/qcri_buckwalter2utf8.sh > $tmpdir/dict/qcri_utf8.txt
 fi
 
-if [ $stage -le 4 ]; then
+if [ $stage -le 6 ]; then
   local/prepare_dict.sh $tmpdir/dict/qcri_utf8.txt
 fi
 
-if [ $stage -le 5 ]; then
+if [ $stage -le 7 ]; then
   # prepare the lang directory
   utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
 fi
 
-if [ $stage -le 6 ]; then
+if [ $stage -le 8 ]; then
   echo "Preparing the subs data for lm training."
   local/subs_prepare_data.pl 
 fi
 
-if [ $stage -le 7 ]; then
+if [ $stage -le 9 ]; then
   echo "lm training."
   local/prepare_lm.sh  $tmpdir/subs/lm/in_vocabulary.txt
 fi
 
-if [ $stage -le 8 ]; then
+if [ $stage -le 10 ]; then
   echo "Making grammar fst."
   utils/format_lm.sh \
     data/lang data/local/lm/trigram.arpa.gz data/local/dict/lexicon.txt \
     data/lang_test
 fi
 
-if [ $stage -le 9 ]; then
+if [ $stage -le 11 ]; then
   # extract acoustic features
   for fld in devtest train test; do
     steps/make_mfcc.sh data/$fld exp/make_mfcc/$fld mfcc
@@ -81,50 +85,20 @@ if [ $stage -le 9 ]; then
   done
 fi
 
-if [ $stage -le 10 ]; then
+if [ $stage -le 12 ]; then
   echo "$0: monophone training"
   steps/train_mono.sh  data/train data/lang exp/mono
 fi
 
-if [ $stage -le 11 ]; then
-  # monophone evaluation
-  (
-    # make decoding graph for monophones
-    utils/mkgraph.sh data/lang_test exp/mono exp/mono/graph
-
-    # test monophones
-    for x in devtest test; do
-      nspk=$(wc -l < data/$x/spk2utt)
-      steps/decode.sh  --nj $nspk exp/mono/graph data/$x exp/mono/decode_${x}
-    done
-  ) &
-fi
-
-if [ $stage -le 12 ]; then
+if [ $stage -le 13 ]; then
   # align with monophones
   steps/align_si.sh  data/train data/lang exp/mono exp/mono_ali
 fi
 
-if [ $stage -le 13 ]; then
+if [ $stage -le 14 ]; then
   echo "$0: Starting  triphone training in exp/tri1"
   steps/train_deltas.sh \
     --boost-silence 1.25 1000 6000 data/train data/lang exp/mono_ali exp/tri1
-fi
-
-wait
-
-if [ $stage -le 14 ]; then
-  # test cd gmm hmm models
-  # make decoding graphs for tri1
-  (
-    utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph
-
-    # decode test data with tri1 models
-    for x in devtest test; do
-      nspk=$(wc -l < data/$x/spk2utt)
-      steps/decode.sh --nj $nspk exp/tri1/graph data/$x exp/tri1/decode_${x}
-    done
-  ) &
 fi
 
 if [ $stage -le 15 ]; then
@@ -139,33 +113,18 @@ if [ $stage -le 16 ]; then
     data/train data/lang exp/tri1_ali exp/tri2b
 fi
 
-wait
-
 if [ $stage -le 17 ]; then
-  (
-    #  make decoding FSTs for tri2b models
-    utils/mkgraph.sh data/lang_test exp/tri2b exp/tri2b/graph
-
-    # decode  test with tri2b models
-    for x in devtest test; do
-      nspk=$(wc -l < data/$x/spk2utt)
-      steps/decode.sh --nj $nspk exp/tri2b/graph data/$x exp/tri2b/decode_${x}
-    done
-  ) &
-fi
-
-if [ $stage -le 18 ]; then
   # align with lda and mllt adapted triphones
   steps/align_si.sh \
     --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali
 fi
 
-if [ $stage -le 19 ]; then
+if [ $stage -le 18 ]; then
   echo "$0: Starting (SAT) triphone training in exp/tri3b"
   steps/train_sat.sh 800 8000 data/train data/lang exp/tri2b_ali exp/tri3b
 fi
 
-if [ $stage -le 20 ]; then
+if [ $stage -le 19 ]; then
   (
     # make decoding graphs for SAT models
     utils/mkgraph.sh data/lang_test exp/tri3b exp/tri3b/graph
@@ -178,13 +137,13 @@ if [ $stage -le 20 ]; then
   ) &
 fi
 
-if [ $stage -le 21 ]; then
+if [ $stage -le 20 ]; then
   # align with tri3b models
   echo "$0: Starting exp/tri3b_ali"
   steps/align_fmllr.sh data/train data/lang exp/tri3b exp/tri3b_ali
 fi
 
-if [ $stage -le 22 ]; then
+if [ $stage -le 21 ]; then
   # train and test chain models
   local/chain2/run_tdnn.sh
 fi
