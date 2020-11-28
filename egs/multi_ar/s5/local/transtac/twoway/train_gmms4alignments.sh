6ff6b5c1b39140c38df5e2efde89269878652604
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-11-28 16:58:15 -0500

    Put combined data under train.

--- egs/multi_ar/s5/local/transtac/twoway/train_gmms4alignments.sh
+++ egs/multi_ar/s5/local/transtac/twoway/train_gmms4alignments.sh
@@ -65,7 +65,7 @@ if [ $stage -le 7 ]; then
   echo "$0 Combine the 7 2way speech corpora."
   twoway_dir=data/local/tmp/transtac/train/twoway
   utils/combine_data.sh \
-      data/transtac_twoway \
+      data/transtac_twoway/train \
     $twoway_dir/appen/2006/lists \
     $twoway_dir/appen/2007/lists \
     $twoway_dir/detroit/2006/lists \
@@ -73,25 +73,25 @@ if [ $stage -le 7 ]; then
     $twoway_dir/nist/2007/lists \
     $twoway_dir/pendleton/2005/lists \
     $twoway_dir/san_diego/2006/lists
-  utils/fix_data_dir.sh data/transtac_twoway || exit 1;
+  utils/fix_data_dir.sh data/transtac_twoway/train || exit 1;
 fi
 
 if [ $stage -le 7 ]; then
     echo "$0: Extract features from transtac 2way data."
   steps/make_mfcc.sh --cmd "$train_cmd" --nj 56 data/transtac_twoway
   steps/compute_cmvn_stats.sh data/transtac_twoway
-  utils/fix_data_dir.sh data/transtac_twoway || exit 1;
+  utils/fix_data_dir.sh data/transtac_twoway/train || exit 1;
 fi
 
 if [ $stage -le 8 ]; then
   echo "$0: Train monophones on transtac 2way Speech."
-  steps/train_mono.sh  --cmd "$train_cmd" --nj 56 data/transtac_twoway \
+  steps/train_mono.sh  --cmd "$train_cmd" --nj 56 data/transtac_twoway/train \
 		       data/lang exp/transtac_twoway/mono || exit 1;
 fi
 
 if [ $stage -le 6 ]; then
   echo "$0: aligning with transtac read monophones"
-  steps/align_si.sh  --cmd "$train_cmd" --nj 56 data/transtac_twoway data/lang \
+  steps/align_si.sh  --cmd "$train_cmd" --nj 56 data/transtac_twoway/train data/lang \
     exp/transtac_twoway/mono exp/transtac_twoway/mono_ali || exit 1;
 fi
 
@@ -99,12 +99,12 @@ if [ $stage -le 7 ]; then
   echo "$0: Starting  transtac read triphone training in exp/gale/tri1."
   steps/train_deltas.sh --cmd "$train_cmd" --boost-silence 1.25 \
     5500 90000 \
-    data/transtac_twoway data/lang exp/transtac_twoway/mono_ali exp/transtac_twoway/tri1 || exit 1;
+    data/transtac_twoway/train data/lang exp/transtac_twoway/mono_ali exp/transtac_twoway/tri1 || exit 1;
 fi
 
 if [ $stage -le 8 ]; then
   echo "$0: Aligning with /transtac read triphones tri1."
-  steps/align_si.sh  --cmd "$train_cmd" --nj 56 data/transtac_twoway data/lang \
+  steps/align_si.sh  --cmd "$train_cmd" --nj 56 data/transtac_twoway/train data/lang \
 		     exp/transtac_twoway/tri1 exp/transtac_twoway/tri1_ali || exit 1;
 fi
 
@@ -113,14 +113,14 @@ if [ $stage -le 9 ]; then
   steps/train_lda_mllt.sh --cmd "$train_cmd" \
     --splice-opts "--left-context=3 --right-context=3" \
     5500 90000 \
-    data/transtac_twoway data/lang exp/transtac_twoway/tri1_ali exp/transtac_twoway/tri2b || exit 1;
+    data/transtac_twoway/train data/lang exp/transtac_twoway/tri1_ali exp/transtac_twoway/tri2b || exit 1;
 fi
 
 if [ $stage -le 10 ]; then
   echo "$0: aligning with transtac read lda and mllt adapted triphones $tri2b."
   steps/align_si.sh  --nj 56 \
     --cmd "$train_cmd" \
-    --use-graphs true data/transtac_twoway data/lang exp/transtac_twoway/tri2b \
+    --use-graphs true data/transtac_twoway/train data/lang exp/transtac_twoway/tri2b \
     exp/transtac_twoway/tri2b_ali || exit 1;
 fi
 
@@ -128,7 +128,7 @@ if [ $stage -le 11 ]; then
   echo "$0: Starting transtac read SAT triphone training in exp/transtac_twoway_appen_2006/tri3b."
   steps/train_sat.sh --cmd "$train_cmd" \
     5500 90000 \
-    data/transtac_twoway data/lang exp/transtac_twoway/tri2b_ali exp/transtac_twoway/tri3b || exit 1;
+    data/transtac_twoway/train data/lang exp/transtac_twoway/tri2b_ali exp/transtac_twoway/tri3b || exit 1;
 fi
 
 if [ $stage -le 12 ]; then
