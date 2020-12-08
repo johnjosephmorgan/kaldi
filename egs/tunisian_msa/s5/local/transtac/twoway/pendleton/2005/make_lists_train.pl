0bd909e52204fd97914935697730eef5791f709c
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-12-08 18:52:54 -0500

    Only write text.

--- egs/tunisian_msa/s5/local/transtac/twoway/pendleton/2005/make_lists_train.pl
+++ egs/tunisian_msa/s5/local/transtac/twoway/pendleton/2005/make_lists_train.pl
@@ -3,7 +3,7 @@
 # Copyright 2019 John Morgan
 # Apache 2.0.
 
-# make_lists_train.pl - write  Kaldi IO lists
+# make_lists_train.pl - Get text for LM training.
 # writes files under data/local/tmp/transtac/train/twoway/pendleton/2005/lists
 
 use strict;
@@ -20,40 +20,14 @@ binmode STDOUT, 'utf8';
 # Initialize variables
 my $tmpdir = "data/local/tmp/transtac/train/twoway/pendleton/2005";
 my $transcripts_file = "$tmpdir/tdf_files.txt";
-# input wav file list
-my $w = "$tmpdir/wav_files.txt";
-# output temporary wav.scp file
-my $wav_scp = "$tmpdir/lists/wav.scp";
-# output temporary utt2spk file
-my $utt_to_spk = "$tmpdir/lists/utt2spk";
 # output temporary text file
 my $txt_out = "$tmpdir/lists/text";
-# temporary segments file
-my $segs = "$tmpdir/lists/segments";
 my $sample_rate = 16000;
 my $data_word_size = 4;
 # done setting variables
 
-# This script looks at 2 files.
-# One containing text transcripts and another containing file names for .wav files.
-# It associates a text transcript with a .wav file name.
-
 system "mkdir -p $tmpdir/lists";
-my %wav_file = ();
-open my $W, '<', $w or croak "problem with $w $!";
-# Store wav file names in hash
-LINE: while ( my $line = <$W> ) {
-  chomp $line;
-  my ($volume,$directories,$file) = File::Spec->splitpath( $line );
-  my $base = basename $file, ".wav";
-  $wav_file{$base} = $line;
-}
-close $W;
-
-open my $WAVSCP, '+>', $wav_scp or croak "problem with $wav_scp $!";
-open my $UTTSPK, '+>', $utt_to_spk or croak "problem with $utt_to_spk $!";
 open my $TXT, '+>:utf8', $txt_out or croak "problem with $txt_out $!";
-open my $SEG, '+>', $segs or croak "problem with $segs $!";
 open my $TR, '<', $transcripts_file or croak "problem with $transcripts_file $!";
 
 while ( my $line = <$TR> ) {
@@ -115,21 +89,9 @@ while ( my $line = <$TR> ) {
     $rec_id = $file;
     my $spk_id = $speaker;
     my $base = $file;
-    if ( defined $wav_file{$base} ) {
-      print $WAVSCP "$rec_id sox -r 22050 -b 16 -e signed \"$wav_file{$base}\" -r 16000 -b 16 -e signed -t .wav - remix 2 |\n";
-    } else {
-      next RECORD;
-    }
-    print $TXT "$utt_id $transcript\n";
-    print $UTTSPK "$utt_id $spk_id\n";
-    print $SEG "$utt_id $rec_id $start $end\n";
+    print $TXT "$transcript\n";
   }
   close $TDF;
 }
 close $TXT;
-close $WAVSCP;
-close $UTTSPK;
-close $W;
-close $SEG;
-
 close $TR;
