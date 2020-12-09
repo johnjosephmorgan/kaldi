4afa7f30ee27b03fb4a3f3b03bf7f8922b168980
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-12-09 14:31:00 -0500

    Make directory.

--- egs/tunisian_msa/s5/local/mflts/get_text.sh
+++ egs/tunisian_msa/s5/local/mflts/get_text.sh
@@ -4,6 +4,7 @@ train_txt_dir=/mnt/corpora/mflts/Modern\ Standard\ Arabic/Speech/annotation/MSA\
 tmpdir=data/local/tmp
 mflts_tmpdir=$tmpdir/mflts
 tmp_train_dir=$mflts_tmpdir/train
+mkdir -p $tmp_train_dir
 
 echo "$0: Getting a list of the   MFLTS MSA training transcript files."
   find "$train_txt_dir" -type f -name "*.tdf" > $tmp_train_dir/tdf_files.txt
