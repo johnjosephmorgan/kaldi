43be69faff66d71444f09d51c11efe4f05dd9ffa
John Joseph Morgan <johnjosephmorgan@gmail.com>
2020-12-29 12:51:47 -0500

    Use 16 instead of 70 jobs.

--- egs/tunisian_msa/s5/local/nnet3/run_common_langs.sh
+++ egs/tunisian_msa/s5/local/nnet3/run_common_langs.sh
@@ -90,7 +90,7 @@ if [ $stage -le 3 ] && [ ! -f data/$lang/${train_set}${feat_suffix}/.done ]; the
     # scale the waveforms, this is useful as we don't use CMVN
     utils/data/perturb_data_dir_volume.sh $data_dir || exit 1;
 
-    steps/make_mfcc${mfcc_affix}.sh --nj 70 $hires_config \
+    steps/make_mfcc${mfcc_affix}.sh --nj 16 $hires_config \
       --cmd "$train_cmd" ${data_dir} $log_dir $mfccdir;
 
     steps/compute_cmvn_stats.sh ${data_dir} $log_dir $mfccdir;
