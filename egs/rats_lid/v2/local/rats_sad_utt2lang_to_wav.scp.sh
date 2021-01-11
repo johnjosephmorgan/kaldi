a850de72be2cf677e20e20245f2c586e37a53b0d
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-11 15:16:09 -0500

    quotes?

--- egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
+++ egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
@@ -10,7 +10,7 @@ done
 for f in dev-1 dev-2 train; do
     {
 	while read line; do
-	    flacfile=$(find $dir -type f -name "${line}.flac")
+	    flacfile=$(find $dir -type f -name "\${line}.flac\")
 	    echo "$line sox ${flacfile} -t wav - remix 1 | " >> data/$f/wav.scp
 	done
     } < data/$f/utt.txt;
