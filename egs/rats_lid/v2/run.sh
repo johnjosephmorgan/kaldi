57d6a68c56ac0250a0b216a9959d3785c7772417
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-10 19:15:18 -0500

    local.

--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -7,7 +7,7 @@ datadir=/mnt/corpora/LDC2015S02/RATS_SAD/data
 local/rats_sad_get_filenames.sh $datadir
 
 for f in dev-1 dev-2 train; do
-    local/rats_sad_data_prep.pl data/annotations/$f.txt
+    local/rats_sad_data_prep.pl data/local/annotations/$f.txt
 done
 
 local/rats_sad_utt2lang_to_wav.scp.sh $datadirr
