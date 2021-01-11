ad7d6052fa83f40a8c6ab758f105f745e9b5de09
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-10 19:32:39 -0500

    Echo variable.

--- egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
+++ egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
@@ -5,7 +5,7 @@ dir=$1
 for f in dev-1 dev-2 train; do
     {
 	while read line; do
-	    utt=$(cut -d " " -f 1 $line)
+	    utt=$(echo $line | cut -d " " -f 1)
 	    flac_file=$(find $dir -type f -name "*.flac" | grep $line)
 	    echo "$utt sox $flac_file -t wav - remix 1 | " >> data/$f/wav.scp
 	done
