30edee3a92a98be3c45242573b96987744fad55f
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-11 14:55:22 -0500

    Get utt list first.

--- egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
+++ egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
@@ -3,12 +3,15 @@
 dir=$1
 
 for f in dev-1 dev-2 train; do
+    cut -d " " -f 1 data/$f/utt2lang > data/$f/utt.txt
+done
+
+
+for f in dev-1 dev-2 train; do
     {
 	while read line; do
-	    line_tmp=$line
-	    utt=$(echo $line | cut -d " " -f 1)
-	    flac_file=$(find $dir -type f -name "*.flac" | xargs grep $tmp_line -)
-	    echo "$utt sox $flac_file -t wav - remix 1 | " >> data/$f/wav.scp
+	    flac_file=$(find $dir -type f -name "${line}.flac")
+	    echo "$line sox $flac_file -t wav - remix 1 | " >> data/$f/wav.scp
 	done
-    } < data/$f/utt2lang;
+    } < data/$f/utt.txt;
 done
