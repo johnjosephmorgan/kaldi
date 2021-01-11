66174aec1b0e56a35e1f7496b818e457b6c19ea4
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-10 19:48:51 -0500

    Quoting.

--- egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
+++ egs/rats_lid/v2/local/rats_sad_utt2lang_to_wav.scp.sh
@@ -5,9 +5,11 @@ dir=$1
 for f in dev-1 dev-2 train; do
     {
 	while read line; do
+	    line_tmp=$line
 	    utt=$(echo $line | cut -d " " -f 1)
-	    flac_file=$(find $dir -type f -name "*.flac" | xargs grep $line -)
+	    flac_file=$(find $dir -type f -name "*.flac" | xargs grep $tmp_line -)
 	    echo "$utt sox $flac_file -t wav - remix 1 | " >> data/$f/wav.scp
 	done
     } < data/$f/utt2lang;
 done
+q
