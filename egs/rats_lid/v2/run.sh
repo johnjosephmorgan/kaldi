659a570382554c9821641491932800d6405e7da9
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-11 12:34:43 -0500

    Extract hires mfcc features.

--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
+. ./cmd.sh
+. ./path.sh
+set -e
+
 datadir=/mnt/corpora/LDC2015S02/RATS_SAD/data
+stage=0
 
-local/rats_sad_get_filenames.sh $datadir
+if [ $stage -le 0 ]; then
+  local/rats_sad_get_filenames.sh $datadir
+fi
 
-for f in dev-1 dev-2 train; do
+if [ $stage -le 1 ]; then
+  for f in dev-1 dev-2 train; do
     local/rats_sad_data_prep.pl data/local/annotations/$f.txt
-done
+  done
+fi
 
-local/rats_sad_utt2lang_to_wav.scp.sh $datadirr
+if [ $stage -le 2 ]; then
+  local/rats_sad_utt2lang_to_wav.scp.sh $datadirr
+fi
+
+if [ $stage -le 3 ]; then
+  for f in dev-1 dev-2 train; do
+    make_mfcc.sh --write-utt2num-frames true --mfcc-config conf/mfcc_hires.conf \
+      --nj 40 --cmd "$train_cmd" data/$f
+    utils/fix_data_dir.sh data/$f
+  done
+fi
