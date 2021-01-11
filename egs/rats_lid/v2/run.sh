5508b91531eb85f0e490f4c8196cd0cd9c306fc9
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-10 19:09:15 -0500

    Arguments to script.

--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -6,6 +6,8 @@ datadir=/mnt/corpora/LDC2015S02/RATS_SAD/data
 
 local/rats_sad_get_filenames.sh $datadir
 
-local/rats_sad_data_prep.pl $f
+for f in dev-1 dev-2 train; do
+    local/rats_sad_data_prep.pl data/annotations/$f.txt
+done
 
 local/rats_sad_utt2lang_to_wav.scp.sh $datadirr
