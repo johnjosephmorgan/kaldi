05d211d53c5a985822a01c30ce777f90addf6e5e
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-12 09:53:49 -0500

    Write inside the loop.

--- egs/rats_lid/v2/local/rats_sad_utt2lang_to_flac_list.sh
+++ egs/rats_lid/v2/local/rats_sad_utt2lang_to_flac_list.sh
@@ -12,7 +12,7 @@ for f in dev-1 dev-2 train; do
   fi
   {
     while read line; do
-      find $dir -type f -name "${line}.flac"
+      find $dir -type f -name "${line}.flac" >> data/$f/flac.txt
     done
-  } < data/$f/utt.txt  >> data/$f/flac.txt;
+  } < data/$f/utt.txt ;
 done
