56d46f765de8d75228e80de9702742c09d942bd5
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-12 09:12:40 -0500

    Style.

--- egs/rats_lid/v2/local/rats_sad_data_prep.pl
+++ egs/rats_lid/v2/local/rats_sad_data_prep.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/env perl
-# rats_sad_data_prep.pl - Prepare utt2lang and wav.scp files
+# rats_sad_data_prep.pl - Prepare utt2lang
 
 use strict;
 use warnings;
 use Carp;
 
+# make the directories
 system "mkdir -p data/dev-1";
 system "mkdir -p data/dev-2";
 system "mkdir -p data/train";
@@ -15,13 +16,14 @@ open my $DEVTWO, '+>', "data/dev-2/utt2lang" or croak "Problem writing file data
 
 my @fields = ();
 while ( my $line=<> ) {
+    chomp $line;
     @fields = split /\t/, $line, 12;
     if ( $fields[0] eq 'train' ) {
 	print $TRAIN "$fields[1] $fields[8]\n";
-	} elsif ( $fields[0] eq 'dev-1' ) {
-	    print $DEVONE "$fields[1] $fields[8]\n";
-	    } elsif ( $fields[0] eq 'dev-2' ) {
-		print $DEVTWO "$fields[1] $fields[8]\n";
+    } elsif ( $fields[0] eq 'dev-1' ) {
+	print $DEVONE "$fields[1] $fields[8]\n";
+    } elsif ( $fields[0] eq 'dev-2' ) {
+	print $DEVTWO "$fields[1] $fields[8]\n";
     } else {
 	croak "Problem with $line $!";
     }
