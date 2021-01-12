32080c35e9288564011de68097da1df48953fcfd
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-12 09:41:12 -0500

    Consolidating commands.

--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -9,26 +9,24 @@ stage=0
 . utils/parse_options.sh
 
 if [ $stage -le 0 ]; then
-  local/rats_sad_get_filenames.sh $datadir
-fi
-
-if [ $stage -le 1 ]; then
-  for f in dev-1 dev-2 train; do
-    local/rats_sad_data_prep.pl data/local/annotations/$f.txt
+  for f in train dev-1 dev-2; do
+    mkdir -p data/$f
+    find $dir/$f/sad -type f -name "*.tab" | xargs cat > data/$f.txt
+    cut -f 2,9 data/$f.txt > data/$f/utt2lang
   done
 fi
 
-if [ $stage -le 2 ]; then
+if [ $stage -le 1 ]; then
   local/rats_sad_utt2lang_to_flac_list.sh $datadirr
 fi
 
-if [ $stage -le 3 ]; then
+if [ $stage -le 2 ]; then
     for f in dev-1 dev-2 train; do
 	local/rats_sad_flac2wav.scp.pl data/$f/flac.txt
     done
 fi
 
-if [ $stage -le 4 ]; then
+if [ $stage -le 3 ]; then
   for f in dev-1 dev-2 train; do
     make_mfcc.sh --write-utt2num-frames true --mfcc-config conf/mfcc_hires.conf \
       --nj 40 --cmd "$train_cmd" data/$f
