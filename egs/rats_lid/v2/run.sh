0904b9c95306175d92a3c55aeaa4e5d50c03e47a
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-12 22:58:44 -0500

    No fixing directory before feature extraction.

--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -36,7 +36,6 @@ fi
 
 if [ $stage -le 4 ]; then
   for f in dev-1 dev-2 train; do
-    utils/fix_data_dir.sh data/$f
     steps/make_mfcc.sh --write-utt2num-frames true --mfcc-config conf/mfcc_hires.conf \
       --nj 40 --cmd "$train_cmd" data/$f
     utils/fix_data_dir.sh data/$f
