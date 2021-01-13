392f95d69b95d18de4fdfaf3dd61589609e35403
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-12 21:12:04 -0500

    Fix directory path for CLSP.

--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -5,7 +5,7 @@
 set -e
 
 #datadir=/mnt/corpora/LDC2015S02/RATS_SAD/data
-datadir=/export/corpora5/LDC/LDC2015S02/RATS_SAD/data
+datadir=/export/corpora5/LDC/LDC2015S02/data
 stage=0
 . utils/parse_options.sh
 
