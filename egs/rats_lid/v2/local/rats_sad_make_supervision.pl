d6f0d25a003bcaf36b6255d9d80e1d162808a592
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-13 08:13:44 -0500

    Try with utt2lang.

---
+++ egs/rats_lid/v2/local/rats_sad_make_supervision.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+# rats_sad_make_supervision.pl - Prepare utt2spl and wav.scp
+
+# This script assumes the following files have been written in previous steps:
+# data/{dev-1,dev-2,train}/flac.txt
+# data/{dev-1,dev-2,train}/utt2lang.txt
+# It writes the following files:
+# data/{dev-1,dev-2,train}/utt2spk
+# data/{dev-1,dev-2,train}/wav.scp
+# data/{dev-1,dev-2,train}/utt2lang
+
+use strict;
+use warnings;
+use Carp;
+
+use File::Basename;
+
+my @folds = ( 'dev-1', 'dev-2', 'train' );
+
+foreach my $f ( @folds ) {
+    open my $FLACS, '<', "data/$f/flac.txt" or croak "Problem with data/$f/flac.txt $!";
+    open my $UTTLANG, '<', "data/$f/utt2lang.txt" or croak "Problem with data/$f/utt2lang.txt $!";
+    open my $WAVSCP, '+>', "data/$f/wav.scp" or croak "Problem with data/$f/wav.scp $!";
+    open my $UTTSPK, '+>', "data/$f/utt2spk" or croak "Problem with data/$f/utt2spk $!";
+    open my $UTTLANG, '+>', "data/$f/utt2lang" or croak "Problem with data/$f/utt2lang $!";
+
+    # store the flacs
+    my %flacs = ();
+    while ( my $line = <$FLACS> ) {
+	chomp $line;
+	my $uid = basename $line, '.flac';
+	$flacs{$uid} = $line;
+    }
+    close $FLACS;
+
+    # store the utterance and language IDs
+    my %utts = ();
+    my %lang = ();
+    while ( my $line = <$UTTLANG> ) {
+	chomp $line;
+	my ($utt,$lang) = split /\s/, $line, 2;
+	$lang{$utt} = $lang;
+	$utts{$utt} = 1;
+    }
+    close $UTTLANG;
+
+    # Write the utt2spk, utt2lang and wav.scp
+    foreach my $utt_id ( keys %utts ) {
+	# Notice that we prepend the language id to the utt id
+	print $WAVSCP "$lang{$utt_id}_${utt_id} sox $flacs{$utt_id} -t wav - |\n";
+	print $UTTSPK "$lang{$utt_id}_${utt_id} $lang{$utt_id}\n";
+	print $UTTLANG "$lang{$utt_id}_${utt_id} $lang{$utt_id}\n";
+    }
+    close $WAVSCP;
+    close $UTTSPK;
+}
--- egs/rats_lid/v2/run.sh
+++ egs/rats_lid/v2/run.sh
@@ -9,33 +9,43 @@ datadir=/export/corpora5/LDC/LDC2015S02/data
 stage=0
 . utils/parse_options.sh
 
+# Retrieve the supervision files and store the data
 if [ $stage -le 0 ]; then
   for f in dev-1 dev-2 train; do
+    echo "$0: Retrieving $f supervision files."
     mkdir -p data/$f
     find $datadir/$f/sad -type f -name "*.tab" | xargs cat > \
       data/$f/annotation.txt
-    cut -f 2,9 data/$f/annotation.txt > data/$f/uttlang.txt
+    echo "$0: Writing utt2lang for $f."
+    cut -f 2,9 data/$f/annotation.txt > data/$f/utt2lang.txt
   done
 fi
 
+# Retrieve the paths to the audio files.
 if [ $stage -le 1 ]; then
   for d in train dev-1 dev-2; do
+    echo "$0: Retrieving paths to audio files."
     find $datadir/$d/audio -type f -name "*.flac" > data/$d/flac.txt
   done
 fi
 
+# Write supervision files. 
 if [ $stage -le 2 ]; then
-    local/rats_sad_make_wav.scp.pl
+  echo "$0: Writing supervision files."
+  local/rats_sad_make_supervision.scp.pl
 fi
 
 if [ $stage -le 3 ]; then
   for x in dev-1 dev-2 train; do
+    echo "$0 Write spk2utt for $x."
     utils/utt2spk_to_spk2utt.pl data/$x/utt2spk > data/$x/spk2utt
   done
 fi
 
+# Extract MFCC features
 if [ $stage -le 4 ]; then
   for f in dev-1 dev-2 train; do
+    echo "$0: Extracting MFCC features for $f."
     utils/fix_data_dir.sh data/$f
     steps/make_mfcc.sh --write-utt2num-frames true --mfcc-config conf/mfcc_hires.conf \
       --nj 40 --cmd "$train_cmd" data/$f
