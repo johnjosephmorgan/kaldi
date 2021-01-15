a729ddf4abd037505dde4ded4e4b1fc539db141f
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-15 14:37:21 -0500

    Add inverse priors?

--- egs/rats_lid/v2/local/run_logistic_regression.sh
+++ egs/rats_lid/v2/local/run_logistic_regression.sh
@@ -50,11 +50,12 @@ utils/sym2int.pl -f 2 $languages \
 #    $prior_scale \
 #    $model_dir/priors.vec
 
-logistic-regression-train --config=$conf "$train_xvectors" \
+logistic-regression-train --config=$conf $train_xvectors \
                           "$classes" $model \
-   2>$model_dir/log/logistic_regression.log
+<   2>$model_dir/log/logistic_regression.log
 
-logistic-regression-copy --scale-priors=$model_dir/priors.vec \
+#logistic-regression-copy --scale-priors=$model_dir/priors.vec \
+    logistic-regression-copy --scale-priors=$model_dir/inv_priors.vec \
    $model $model_rebalanced
 
 logistic-regression-eval --apply-log=$apply_log $model \
