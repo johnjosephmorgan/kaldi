4d13b64d5dad39513584a11e4df1c5fdbe725e90
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-17 18:54:31 -0500

    Remove extra directory level.

--- egs/rats_lid/v2/run_sad.sh
+++ egs/rats_lid/v2/run_sad.sh
@@ -57,27 +57,27 @@ mkdir -p $working_dir
 {
   while read src; do
       base=$(basename $src .$input_extension)
-      mkdir -p speechactivity/$base
+      mkdir -p $working_dir/$base
     echo "$0 Stage 0: Write parameter files for Kaldi SAD."
     # wav.scp
-    echo "$base sox -t $input_extension $src -t wav -r $sad_sampling_rate -b 16 - channels 1 |"> $working_dir/speechactivity/wav.scp
+    echo "$base sox -t $input_extension $src -t wav -r $sad_sampling_rate -b 16 - channels 1 |"> $working_dir/wav.scp
     # the utt2spk file is simple since we process 1 recording 
-    echo "$base $base" > $working_dir/speechactivity/utt2spk
+    echo "$base $base" > $working_dir/utt2spk
     # spk2utt
-    echo "$base $base" > $working_dir/speechactivity/spk2utt
+    echo "$base $base" > $working_dir/spk2utt
 
     echo "$0 Stage 1: Waveform Preprocessing"
     echo "Extract MFCC feature vectors for SAD."
-    run.pl  $working_dir/speechactivity/log/make_mfcc_hires.log \
+    run.pl  $working_dir/log/make_mfcc_hires.log \
   compute-mfcc-feats \
-    --write-utt2dur=ark,t:$working_dir/speechactivity/utt2dur \
+    --write-utt2dur=ark,t:$working_dir/utt2dur \
     --config=$mfcc_hires_config \
-    scp,p:$working_dir/speechactivity/wav.scp ark:- '|' copy-feats \
-    --write-num-frames=ark,t:$working_dir/speechactivity/utt2num_frames --compress=$compress \
-    ark:- ark,t,scp:$(pwd)/$working_dir/speechactivity/raw_mfcc.txt,$(pwd)/$working_dir/speechactivity/raw_mfcc.scp || exit 1;
+    scp,p:$working_dir/wav.scp ark:- '|' copy-feats \
+    --write-num-frames=ark,t:$working_dir/utt2num_frames --compress=$compress \
+    ark:- ark,t,scp:$(pwd)/$working_dir/raw_mfcc.txt,$(pwd)/$working_dir/raw_mfcc.scp || exit 1;
 
 echo "$0 stage 2: Segmentation: Propagate features through the raw SAD neural network model."
-run.pl  $working_dir/speechactivity/log/propagate_features_thru_nnet.log \
+run.pl  $working_dir/log/propagate_features_thru_nnet.log \
   nnet3-compute --use-gpu=no \
     --extra-left-context=$extra_left_context \
     --extra-left-context-initial=$extra_left_context_initial \
@@ -86,14 +86,14 @@ run.pl  $working_dir/speechactivity/log/propagate_features_thru_nnet.log \
     --frame-subsampling-factor=$frame_subsampling_factor \
     --frames-per-chunk=$frames_per_chunk \
     $sad_nnet_dir/final.raw \
-    "ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$working_dir/speechactivity/utt2spk scp:$working_dir/speechactivity/raw_mfcc.scp scp:$working_dir/speechactivity/raw_mfcc.scp ark:- |" \
-    "ark:| copy-matrix --apply-exp ark:- ark,t,scp:$working_dir/speechactivity/output.txt,$working_dir/speechactivity/output.scp" || exit 1;
+    "ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$working_dir/utt2spk scp:$working_dir/raw_mfcc.scp scp:$working_dir/raw_mfcc.scp ark:- |" \
+    "ark:| copy-matrix --apply-exp ark:- ark,t,scp:$working_dir/output.txt,$working_dir/output.scp" || exit 1;
 
-cp $working_dir/speechactivity/raw_mfcc.scp $working_dir/speechactivity/feats.scp
+cp $working_dir/raw_mfcc.scp $working_dir/feats.scp
 
 echo "$0 Stage 3: Write a file containing the targets that will be used to make the HCLG.fst."
-mkdir -p $working_dir/speechactivity/graph_output
-cat <<EOF > $working_dir/speechactivity/graph_output/words.txt
+mkdir -p $working_dir/graph_output
+cat <<EOF > $working_dir/graph_output/words.txt
 <eps> 0
 silence 1
 speech 2
@@ -101,48 +101,48 @@ EOF
 
 frame_shift=0.03
 echo "$0 Stage 4: Make the HCLG.fst for SAD."
-run.pl $working_dir/speechactivity/graph_output/log/make_graph.log \
+run.pl $working_dir/graph_output/log/make_graph.log \
   local/prepare_sad_graph.py \
   --frame-shift=$frame_shift \
   --max-speech-duration=$max_speech_duration \
   --min-silence-duration=$min_silence_duration \
   --min-speech-duration=$min_speech_duration \
   - '|' fstcompile \
-  --isymbols=$working_dir/speechactivity/graph_output/words.txt \
-  --osymbols=$working_dir/speechactivity/graph_output/words.txt '>' $working_dir/speechactivity/graph_output/HCLG.fst
+  --isymbols=$working_dir/graph_output/words.txt \
+  --osymbols=$working_dir/graph_output/words.txt '>' $working_dir/graph_output/HCLG.fst
 
 echo "$0 Stage 5: Get the matrix of probability transforms."
-steps/segmentation/internal/get_transform_probs_mat.py --priors=$sad_nnet_dir/post_output.vec --sil-scale=$sil_scale > $working_dir/speechactivity/transform_probs.mat
+steps/segmentation/internal/get_transform_probs_mat.py --priors=$sad_nnet_dir/post_output.vec --sil-scale=$sil_scale > $working_dir/transform_probs.mat
 
 echo "$0 Stage 6: Run viterbi alignment."
-run.pl $working_dir/speechactivity/log/get_viterbi_alignments.log \
+run.pl $working_dir/log/get_viterbi_alignments.log \
   decode-faster \
   --acoustic-scale=$acoustic_scale \
   --beam=$beam \
   --binary=false \
   --max-active=$max_active \
   --min-active=$min_active \
-  $working_dir/speechactivity/graph_output/HCLG.fst \
-  "ark:cat $working_dir/speechactivity/output.scp | copy-feats scp:- ark:- | transform-feats $working_dir/speechactivity/transform_probs.mat ark:- ark:- | copy-matrix --apply-log ark:- ark:- |" \
+  $working_dir/graph_output/HCLG.fst \
+  "ark:cat $working_dir/output.scp | copy-feats scp:- ark:- | transform-feats $working_dir/transform_probs.mat ark:- ark:- | copy-matrix --apply-log ark:- ark:- |" \
   ark:/dev/null \
-  "ark,t:$working_dir/speechactivity/ali.1"
+  "ark,t:$working_dir/ali.1"
 
-gzip $working_dir/speechactivity/ali.1
+gzip $working_dir/ali.1
 
-echo "1" > $working_dir/speechactivity/num_jobs
+echo "1" > $working_dir/num_jobs
 echo "$0 Stage 7: Get the segments from the alignments."
 steps/segmentation/post_process_sad_to_segments.sh     --segment-padding 0.2 \
   --min-segment-dur $min_segment_dur     \
   --merge-consecutive-max-dur $merge_consecutive_max_dur     --cmd run.pl \
-  --frame-shift $(perl -e "print 3 * 0.01")     $working_dir/speechactivity \
-  $working_dir/speechactivity $working_dir/speechactivity 
+  --frame-shift $(perl -e "print 3 * 0.01")     $working_dir \
+  $working_dir $working_dir 
 
-mv $working_dir/speechactivity/segments $working_dir/speechactivity/segs
+mv $working_dir/segments $working_dir/segs
 echo "$0 Stage 8: Get subsegments."
-utils/data/subsegment_data_dir.sh $working_dir/speechactivity \
-  $working_dir/speechactivity/segments.1 $working_dir/speechactivity/subsegments
+utils/data/subsegment_data_dir.sh $working_dir \
+  $working_dir/segments.1 $working_dir/subsegments
 
 echo "$0 Stage 9: Make .wav files from segmentation."
-local/speechactivity2wav.pl $src
+local.pl $src
   done
 } < $list;
