41d16d372f6f4a115512aebb21554e3e8b8e6571
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-01-26 20:14:02 -0500

    Do not check for tree existence.

--- egs/ultra_testbed/t1/run.sh
+++ egs/ultra_testbed/t1/run.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
 # This recipe runs a decoder test on recordings in the following directory:
+
+# Start setting configuration variables and parameters
 datadir=$PWD/Libyan_msa_arl
 speakers=(adel anwar bubaker hisham mukhtar redha srj yousef)
+acoustic_scale=0.1
+beam=1.0
+do_endpointing=false
+extra_left_context_initial=0
+frame_subsampling_factor=3
+frames_per_chunk=50
+lattice_beam=1.0
+max_active=2147483647
+min_active=200
+online=true
+# Finished setting variables and parameters
 . ./path.sh
 stage=0
 . utils/parse_options.sh
@@ -19,7 +32,7 @@ fi
 src=$1
 
 # Check that resources exist
-for f in HCLG.fst final.mdl tree words.txt; do
+for f in HCLG.fst final.mdl words.txt; do
   echo "Checking $f." 
   [ ! -f $src/$f ] && echo "$f is missing." && exit 1;
 done
@@ -47,6 +60,7 @@ if [ $stage -le 1 ]; then
   done
 fi
 
+# Combin directories into 1 test directory. This is probably unnecessary.
 if [ $stage -le 2 ]; then
   utils/combine_data.sh data/test data/adel data/anwar data/bubaker data/hisham \
     data/mukhtar data/redha data/srj data/yousef
@@ -59,18 +73,18 @@ if [ $stage -le 3 ]; then
     mkdir -p $src/decode_online/$s/log
     run.pl $src/decode_online/$s/log/decode.log \
       online2-wav-nnet3-latgen-faster \
-        --acoustic-scale=0.1 \
-        --beam=10.0 \
+        --acoustic-scale=$acoustic_scale \
+        --beam=$beam \
         --config=$src/conf/online.conf \
-        --do-endpointing=false \
-        --extra-left-context-initial=0 \
-        --frame-subsampling-factor=3 \
-        --frames-per-chunk=150 \
+        --do-endpointing=$do_endpointing \
+        --extra-left-context-initial=$extra_left_context_initial \
+        --frame-subsampling-factor=$frame_subsampling_factor \
+        --frames-per-chunk=$frames_per_chunk \
 	--ivector-extraction-config=$src/conf/ivector_extractor.conf \
-        --lattice-beam=4.0 \
-        --max-active=2147483647 \
-        --min-active=200 \
-        --online=true \
+        --lattice-beam=$lattice_beam \
+        --max-active=$max_active \
+        --min-active=$min_active \
+        --online=$online \
         --word-symbol-table=$src/words.txt \
         $src/final.mdl \
         $src/HCLG.fst \
