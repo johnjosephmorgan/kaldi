5daeb2e759264844cfb68b146f0a8564dbaccca1
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-02-13 18:06:45 -0500

    Typo.

--- egs/mini_librispeech/s5b/run.sh
+++ egs/mini_librispeech/s5b/run.sh
@@ -532,7 +532,7 @@ if [ $stage -le 20 ]; then
   tree_dir=exp/mini_librispeech/tree
   utils/mkgraph.sh \
     --self-loop-scale 1.0 \
-    data/mini_librispeech/data/lang_nosp_test_tgsmall \
+    data/mini_librispeech/lang_nosp_test_tgsmall \
     $tree_dir \
     $tree_dir/graph_tgsmall || exit 1;
 fi
