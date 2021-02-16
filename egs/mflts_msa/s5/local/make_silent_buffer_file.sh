0d1f9892386e233f84d878fbe70b32fce63f3dac
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-02-16 13:44:06 -0500

    Fixed loops.

--- egs/mflts_msa/s5/local/make_silent_buffer_file.sh
+++ egs/mflts_msa/s5/local/make_silent_buffer_file.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
 overlap=20
+
 for r in out_diarized/work/*; do
-  for w in $r/*; do
-    local/make_silent_buffer_file.pl \
-      $w \
-      out_diarized/tmp/samples.txt \
-      $overlap
+  for s in $r/audio_threshold/*; do
+    for w in $s/*; do
+      local/make_silent_buffer_file.pl \
+        $w \
+        $overlap
+    done
   done
 done
