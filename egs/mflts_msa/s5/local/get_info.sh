718b3184980e7bf62a3a8a8a0a1cb048a15b016e
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-02-16 13:47:30 -0500

    Corrected names.

--- egs/mflts_msa/s5/local/get_info.sh
+++ egs/mflts_msa/s5/local/get_info.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-if [ ! -d out_diarized/tmp ]; then
-    mkdir -p out_diarized/tmp
-fi
-
-if [ -f out_diarized/tmp/info.txt ]; then
-  rm out_diarized/tmp/info.txt
-fi
+mkdir -p out_diarized/tmp
 
 for r in out_diarized/work/*; do
   for s in $r/audio_threshold/*; do
     for f in $s/*; do
       sox --i $f | egrep "Input|Duration" >> out_diarized/tmp/info.txt
+      base=$(basename $f .wav)
+      dir=$(dirname $f)
+      sox --i $f | egrep "Input|Duration" > $dir/${base}_info.txt
+      cat $dir/${base}_info.txt | grep Duration | cut -d "=" -f 2 | cut -d "~" -f 1 | cut -d " " -f 2 > $dir/${base}_duration.txt
+      cat $dir/${base}_info.txt | grep "Input" | cut -d ":" -f 2 | tr -d "'" > $dir/${base}_filename.txt
+      paste $dir/${base}_filename.txt $dir/${base}_duration.txt > $dir/${base}_samples.txt
     done
   done
 done
