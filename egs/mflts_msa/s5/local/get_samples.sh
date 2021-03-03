2433413939c6943c30f23aa4f09ac0fdbc874615
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-03-03 08:57:36 -0500

    Script to write the files containing the number of samples.

---
+++ egs/mflts_msa/s5/local/get_samples.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script makes the files containing the number of samples
+
+# loop over each speaker directory
+for s in out_diarized/speakers/*; do
+  # loop over each segment for the current speaker
+    for f in $s/*.wav; do
+      # Get the basename for the current audio file
+      base=$(basename $f .wav)
+      # get the path to the current speaker directory
+      spk_path=$(dirname $f)
+      # get the current speaker id
+      spk=$(basename $spk_path)
+      # make a name for the directory where we will store the work for the current audio files
+      dir=out_diarized/work/speakers/$spk
+      # put filename and duration into samples.txt
+      paste $dir/${base}_filename.txt $dir/${base}_duration.txt > $dir/${base}_samples.txt
+  done
+done
