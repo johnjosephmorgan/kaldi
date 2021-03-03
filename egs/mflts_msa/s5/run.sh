04a23fa502e3d80e2ea9ce6db0eb3cd54e4bc192
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-03-03 08:59:35 -0500

    Loops.

--- egs/mflts_msa/s5/run.sh
+++ egs/mflts_msa/s5/run.sh
@@ -42,6 +42,8 @@ if [ $stage -le 4 ]; then
 fi
 
 if [ $stage -le 5 ]; then
+  # Write the duration information to file
+  # We ran the previous stage separately because it uses sox 
   local/get_samples.sh
 fi
 
@@ -49,12 +51,13 @@ if [ $stage -le 6 ]; then
   mkdir -p out_diarized/overlaps
   n=$(find out_diarized/work/speakers -type f -name "*_samples.txt" | wc -l)
   echo "There are $n sample files."
+  # Loop a lot of times
   for ((i=0;i<=n;i++)); do
     # randomly choose files to process
     s1=$(find out_diarized/work/speakers -type f -name "*_samples.txt" | shuf -n 1)
     s2=$(find out_diarized/work/speakers -type f -name "*_samples.txt" | shuf -n 1)
     local/overlap.sh $s1 $s2
-    rm $s1 $s2
+    #rm $s1 $s2
   done
 fi
 
