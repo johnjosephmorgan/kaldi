78a5f7aa2f54518fdf39c73ed506c711b9a0277e
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-03-10 09:20:22 -0500

    Express markers in seconds.

--- egs/mflts_msa/s5/local/pairs2rttm.pl
+++ egs/mflts_msa/s5/local/pairs2rttm.pl
@@ -33,14 +33,20 @@ while ( my $line = <$PAIRS> ) {
     my ($a_1,$a_2,$a_3) = split /\<NA\>(SPEAKER)/, $line, 2;
     # There are 4 relevant fields: rec_id, start, duration, and speaker id
     my ($type_1,$rec_id_1,$chn_1,$begin_1,$dur_1,$foo_1,$foo_2,$spk_1,$foo_3) = split /\s+/, $a_1, 9;
+    # We need the times in seconds
+    my $start_1_in_seconds = $start_1 / 32000;
+    my $dur_1_in_seconds = $dur_1 / 32000;
     # WRite the fields for the first segment on 1 line
-    print $RTTM "SPEAKER $rec_id_1 $chn_1 $start_1 $dur_1 \<NA\> \<NA\> $spk_1 \<NA\> \<NA\>\n";
+    print $RTTM "SPEAKER $rec_id_1 $chn_1 $start_1_in_seconds $dur_1 \<NA\> \<NA\> $spk_1 \<NA\> \<NA\>\n";
     # get the fields for the  second segment
     my ($empty,$rec_id_2,$chn_2,$begin_2,$dur_2,$foo_5,$foo_6,$spk_2,$foo_7,$foo_8) = split /\s+/, $a_3, 9;
     # update start for segment 2
     $start_2 += $begin_2;
+    # We need the times in seconds
+    my $start_2_in_seconds = $start_2 / 32000;
+    my $dur_2_in_seconds = $dur_2 / 32000;
     # write the fields for the second segment on the next line
-    print $RTTM "SPEAKER $rec_id_2 $chn_2 $start_2 $dur_2 \<NA\> \<NA\> $spk_2 \<NA\> \<NA\>\n";
+    print $RTTM "SPEAKER $rec_id_2 $chn_2 $start_2_in_seconds $dur_2_in_seconds \<NA\> \<NA\> $spk_2 \<NA\> \<NA\>\n";
     # set end time
     my $end = $begin_2 + $dur_2;
     # update begin time for segment 1
