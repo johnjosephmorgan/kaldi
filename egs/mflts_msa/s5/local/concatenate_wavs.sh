1afca5f05b0e160f5357d56c376afa4a012db864
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-03-13 16:01:37 -0500

    Use work instead of out_diarized.

--- egs/mflts_msa/s5/local/concatenate_wavs.sh
+++ egs/mflts_msa/s5/local/concatenate_wavs.sh
@@ -5,10 +5,10 @@ i=$1
 # set the number of pairs to concatenate
 n=100
 # make the output directory
-mkdir -p out_diarized/concats/$i
+mkdir -p work/concats/$i
 
 # find segment pairs to concatenate
-a=$(find out_diarized/overlaps -type f -name "max.wav" | shuf -n $n)
+a=$(find work/overlaps -type f -name "max.wav" | shuf -n $n)
 
 # check that the files exist
 for m in ${a[@]}; do
@@ -16,35 +16,35 @@ for m in ${a[@]}; do
 done
 
 # concatenate all the segments
-$(sox ${a[@]} out_diarized/concats/$i/overlap.wav) || exit 1;
+$(sox ${a[@]} work/concats/$i/overlap.wav) || exit 1;
 
 # Concatenate rttm files
-[ -f out_diarized/concats/$i/pairs.txt ] && rm out_diarized/concats/$i/pairs.txt;
+[ -f work/concats/$i/pairs.txt ] && rm work/concats/$i/pairs.txt;
 for m in ${a[@]}; do
   markerpath=$(dirname "$m")
   rttm=$(cat "$markerpath/overlap.rttm")
-  echo $rttm   >> out_diarized/concats/$i/pairs.txt
+  echo $rttm   >> work/concats/$i/pairs.txt
 done
 
 # write list of wav file names
 for m in "${a[@]}"; do
-  printf '%s\n' $m > out_diarized/concats/$i/wavs.txt
+  printf '%s\n' $m > work/concats/$i/wavs.txt
 done
 
 # write list of start markers
-[ -f out_diarized/concats/${i}_starts.txt ] && rm out_diarized/concats/${i}_starts.txt;
+[ -f work/concats/${i}_starts.txt ] && rm work/concats/${i}_starts.txt;
 total=0
 for m in ${a[@]}; do
   markerpath=$(dirname "$m")
   start=$(cat $markerpath/segment_2_start.txt)
   ((totalsum+=total))
   ((new_start=start + totalsum))
-  printf '%s\n' "$new_start" >> out_diarized/concats/$i/starts.txt
+  printf '%s\n' "$new_start" >> work/concats/$i/starts.txt
   total=$(cat $markerpath/segment_2_end.txt)
 done
 
 # write list of end markers
-#[ -f out_diarized/concats/$i/ends.txt ] && rm out_diarized/concats/$i/ends.txt;
+#[ -f work/concats/$i/ends.txt ] && rm work/concats/$i/ends.txt;
 #total=0
 #totalsum=0
 #for m in ${a[@]}; do
@@ -52,27 +52,27 @@ done
 #  end=$(cat $markerpath/end.txt)
 #  ((totalsum+=total))
 #  ((new_end=end + totalsum))
-#  printf '%s\n' "$new_end" >> out_diarized/concats/$i/ends.txt
+#  printf '%s\n' "$new_end" >> work/concats/$i/ends.txt
 #  total=$(cat $markerpath/total.txt)
 #done
 
 # write list of overlap durations
-[ -f out_diarized/concats/$i/durs.txt ] && rm out_diarized/concats/$i/durs.txt;
+[ -f work/concats/$i/durs.txt ] && rm work/concats/$i/durs.txt;
 for m in ${a[@]}; do
   markerpath=$(dirname "$m")
   dur=$(cat $markerpath/overlap_duration.txt)
-  printf '%s\n' "$dur" >> out_diarized/concats/$i/durs.txt
+  printf '%s\n' "$dur" >> work/concats/$i/durs.txt
 done
 
 # write list of segment pair total durations
-[ -f out_diarized/concats/$i/total_segment_pair_durations.txt ] && rm out_diarized/concats/$i/total_segment_pair_durations.txt;
+[ -f work/concats/$i/total_segment_pair_durations.txt ] && rm work/concats/$i/total_segment_pair_durations.txt;
 for m in ${a[@]}; do
   markerpath=$(dirname "$m")
   total=$(cat $markerpath/segment_2_end.txt)
-  printf '%s\n' "$total" >> out_diarized/concats/$i/total_segment_pair_durations.txt
+  printf '%s\n' "$total" >> work/concats/$i/total_segment_pair_durations.txt
 done
 
-paste out_diarized/concats/$i/wavs.txt out_diarized/concats/$i/starts.txt out_diarized/concats/$i/durs.txt > out_diarized/concats/$i/segment_info.txt
+paste work/concats/$i/wavs.txt work/concats/$i/starts.txt work/concats/$i/durs.txt > work/concats/$i/segment_info.txt
 
 # remove the process files
 # We do not want to use them again
