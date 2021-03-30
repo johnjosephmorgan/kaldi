f5207cea1a0a26dafc570f509694de9f644119a5
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-03-30 12:02:56 -0400

    Remove comment.

--- egs/ami/s5c/local/prepare_data.py
+++ egs/ami/s5c/local/prepare_data.py
@@ -47,11 +47,10 @@ def write_segments(sad_labels_dir, output_path):
                     parts = line.strip().split()
                     start = float(parts[0])
                     end = float(parts[1])
-                    #seg_id = f'{file_id}_{100*start:06.0f}_{100*end:06.0f}'
-                    #f.write(f'{seg_id} {file_id} {start} {end}\n')
+                    seg_id = f'{file_id}_{100*start:06.0f}_{100*end:06.0f}'
+                    f.write(f'{seg_id} {file_id} {start} {end}\n')
 
 def make_diar_data(meetings, wav_path, output_path, sad_labels_dir=None):
-    print('meetings', meetings, 'wav path', wav_path, 'output path', output_path, 'sad labels dir', sad_labels_dir)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
