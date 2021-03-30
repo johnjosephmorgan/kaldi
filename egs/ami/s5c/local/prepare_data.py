5ea02243c03f31318afa986ed0f12db15a351b20
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-03-30 12:07:04 -0400

    Typo.

--- egs/ami/s5c/local/prepare_data.py
+++ egs/ami/s5c/local/prepare_data.py
@@ -48,7 +48,7 @@ def write_segments(sad_labels_dir, output_path):
                     start = float(parts[0])
                     end = float(parts[1])
                     #seg_id = f'{file_id}_{100*start:06.0f}_{100*end:06.0f}'
-                    seg_id  {file_id + start + end
+                    seg_id  = file_id + start + end
                     f.write(f'{seg_id} {file_id} {start} {end}\n')
 
 def make_diar_data(meetings, wav_path, output_path, sad_labels_dir=None):
