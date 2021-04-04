e7e01d75388b0c1d4af7e3b6ecda685b335c07d0
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-04 17:28:27 -0400

    Removed exit.

--- egs/tunisian_msa/s5a/run.sh
+++ egs/tunisian_msa/s5a/run.sh
@@ -39,7 +39,7 @@ if [ $stage -le 3 ]; then
     utils/fix_data_dir.sh data/$fld
   done
 fi
-exit
+
 if [ $stage -le 4 ]; then
   echo "$0: monophone training"
   steps/train_mono.sh  data/train data/lang exp/mono
