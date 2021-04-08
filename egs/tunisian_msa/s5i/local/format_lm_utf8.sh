998546d83b266856a664a6e04f6846e268061e28
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-08 15:27:07 -0400

    Copied wrong script.

--- egs/tunisian_msa/s5i/local/format_lm_utf8.sh
+++ egs/tunisian_msa/s5i/local/format_lm_utf8.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
+/usr/bin/env bash
+
 utils/format_lm.sh \
   data/lang \
-  data/local/lm/$LM \
+  data/local/lm/gale.o3g.kn_utf8.gz \
   data/local/dict/lexicon.txt \
-  data/lang_test || exit 1;
+  data/lang_test
