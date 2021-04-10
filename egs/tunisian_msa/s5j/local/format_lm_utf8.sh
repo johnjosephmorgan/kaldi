d934a7a734b4ed6bb880246d46428ea873e41737
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-10 11:53:36 -0400

    Correcting paths.

--- egs/tunisian_msa/s5j/local/format_lm_utf8.sh
+++ egs/tunisian_msa/s5j/local/format_lm_utf8.sh
@@ -1,7 +1,7 @@
 /usr/bin/env bash
 
 utils/format_lm.sh \
-  data/lang \
+  data/gale_arabic/lang \
   data/local/lm/gale.o3g.kn_utf8.gz \
-  data/local/dict/lexicon.txt \
+  ../s5a/data/local/dict/lexicon.txt \
   data/lang_test
