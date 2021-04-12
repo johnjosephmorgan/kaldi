1d002a264f6a3d5ead5af33594926e3faf62f269
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-12 08:47:10 -0400

    Use lang from TAMSA.

--- egs/tunisian_msa/s5j/local/format_lm_utf8.sh
+++ egs/tunisian_msa/s5j/local/format_lm_utf8.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 utils/format_lm.sh \
-  data/gale_arabic/lang \
+  data/tunisian_msa/lang \
   data/local/lm/gale.o3g.kn_utf8.gz \
   ../s5a/data/local/dict/lexicon.txt \
   data/lang_test
