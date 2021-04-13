ecf122fefd474aec6c2e1173057aa2ed0d76ca81
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-13 19:31:03 -0400

    Directory paths.

--- egs/tunisian_msa/s5k/local/format_lm_utf8.sh
+++ egs/tunisian_msa/s5k/local/format_lm_utf8.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 utils/format_lm.sh \
-  data/tunisian_msa/lang \
+  data/lang \
   data/local/lm/gale.o3g.kn_utf8.gz \
-  ../s5a/data/local/dict/lexicon.txt \
+  data/local/dict/lexicon.txt \
   data/lang_test
