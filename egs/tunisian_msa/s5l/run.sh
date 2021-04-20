efcdb456c92a93afb2d1339b8cae41518159ca4a
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-20 09:47:35 -0400

    Delete lm directory.

--- egs/tunisian_msa/s5l/run.sh
+++ egs/tunisian_msa/s5l/run.sh
@@ -694,7 +694,8 @@ fi
 if [ $stage -le 25 ]; then
   # Get the ARL LM training text
   # The data is located under /mnt/corpora/ultra/arabic_lm_text/ar
-  # The data is written to data/local/lm/training_text.txt
+    # The data is written to data/local/lm/training_text.txt
+    [ ! -d data/local/lm ] || rm -Rf data/local/lm;
   local/get_arl_lm_training_text.sh
   # Append the Gale arabic training text.
   cat     ../../gale_arabic/s5d/data/train/text >> data/local/lm/training_text.txt
