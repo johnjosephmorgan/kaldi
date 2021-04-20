a87315e9c86615d7eeda68a7acdd53a9bc99e094
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-20 09:32:58 -0400

    Delete existing file.

--- egs/tunisian_msa/s5l/local/get_arl_lm_training_text.sh
+++ egs/tunisian_msa/s5l/local/get_arl_lm_training_text.sh
@@ -3,7 +3,7 @@
 arl_lm_data_path=/mnt/corpora/ultra/arabic_lm_text/ar
 
 [ -d data/local/lm ] || mkdir -p data/local/lm;
-
+[ -f data/local/lm/training_text.txt ] || rm data/local/lm/training_text.txt;
 for f in fm5.0_cleaned_ZA_ar fm6.0_cleaned_ZA_ar fm6.22_cleaned_ZA_ar FM7-8_cleaned_ZA_ar mflts_msa_ar MNSTC-I_cleaned_ZA_ar; do
   cat $arl_lm_data_path/$f.txt >> data/local/lm/training_text.txt
 done
