bb50b350cb43fe904a832b73c83b989faa96d0d4
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-21 15:39:13 -0400

    Easier logic.

--- egs/tunisian_msa/s5l/run.sh
+++ egs/tunisian_msa/s5l/run.sh
@@ -705,8 +705,8 @@ if [ $stage -le 25 ]; then
   lexicon=../../gale_arabic/s5d/data/local/dict/lexicon.txt
   dir=data/local/lm
   $(dos2unix $dir/training_arl_text_utf8.txt)
-  [ ! -f $gale_training_text ] && echo "$0: No such file $gale_training_text" && exit 1;
-  [ ! -f $lexicon ] && echo "$0: No such file $lexicon" && exit 1;
+  [ -f $gale_training_text ] || echo "$0: No such file $gale_training_text" && exit 1;
+  [ -f $lexicon ] || echo "$0: No such file $lexicon" && exit 1;
   loc=$(which ngram-count);
   if [ -z $loc ]; then
     if uname -a | grep 64 >/dev/null; then # some kind of 64 bit...
