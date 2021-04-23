b2a79435dd523645ffb6907257ccec1e15671964
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-04-23 14:45:51 -0400

    Initial commit of s5o. Add ARL lm training text.

---
+++ egs/tunisian_msa/s5o/local/gale_arl_train_lms_utf8.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+help_message="Usage: $0 [options] <train-txt> <dict> <out-dir>
+Train language models for GALE Arabic.\n
+options: 
+  --help          # print this message and exit
+";
+
+. utils/parse_options.sh
+
+if [ $# -lt 3 ]; then
+  printf "$help_message\n";
+  exit 1;
+fi
+
+text=$1     # data/local/train/text
+lexicon=$2  # data/local/dict/lexicon.txt
+dir=$3      # data/local/lm
+arl_lm_data_path=/mnt/corpora/ultra/arabic_lm_text/ar
+
+for f in "$text" "$lexicon"; do
+  [ ! -f $x ] && echo "$0: No such file $f" && exit 1;
+done
+
+loc=`which ngram-count`;
+if [ -z $loc ]; then
+  if uname -a | grep 64 >/dev/null; then # some kind of 64 bit...
+    sdir=`pwd`/../../../tools/srilm/bin/i686-m64 
+  else
+    sdir=`pwd`/../../../tools/srilm/bin/i686
+  fi
+  if [ -f $sdir/ngram-count ]; then
+    echo Using SRILM tools from $sdir
+    export PATH=$PATH:$sdir
+  else
+    echo You appear to not have SRILM tools installed, either on your path,
+    echo or installed in $sdir.  See tools/install_srilm.sh for installation
+    echo instructions.
+    exit 1
+  fi
+fi
+
+stage=0
+
+set -o errexit
+mkdir -p $dir
+export LC_ALL=C 
+
+heldout_sent=10000
+cut -d' ' -f2- $text | gzip -c > $dir/train.all.gz
+cut -d' ' -f2- $text | tail -n +$heldout_sent | gzip -c > $dir/train.gz
+cut -d' ' -f2- $text | head -n $heldout_sent > $dir/heldout
+
+# convert the heldout to utf8
+local/buckwalter2unicode.py \
+  -i $dir/heldout \
+  -o $dir/heldout_utf8.txt 
+
+cut -d' ' -f1 $lexicon > $dir/wordlist.txt
+
+# convert the training text to utf8
+gunzip   $dir/train.gz 
+local/buckwalter2unicode.py \
+  -i $dir/train \
+  -o $dir/train_utf8.txt
+
+  # get the ARL lm training text
+  for f in fm5.0_cleaned_ZA_ar fm6.0_cleaned_ZA_ar fm6.22_cleaned_ZA_ar FM7-8_cleaned_ZA_ar mflts_msa_ar MNSTC-I_cleaned_ZA_ar; do
+    cat $arl_lm_data_path/$f.txt >> $dir/train_utf8.txt
+  done
+  # get rid of the carriage returns
+  $(dos2unix $dir/train_utf8.txt)
+
+gzip $dir/train_utf8.txt
+
+if [ $stage -le 1 ]; then
+  # Trigram language model
+  echo "training tri-gram lm"
+  smoothing="kn"
+  ngram-count \
+    -text $dir/train_utf8.txt.gz \
+    -order 3 \
+    -limit-vocab -vocab $dir/wordlist.txt \
+    -unk -map-unk "<UNK>" \
+    -${smoothing}discount -interpolate \
+    -lm $dir/gale.o3g.${smoothing}_utf8.gz
+  echo "PPL for GALE Arabic trigram LM:"
+  ngram \
+    -unk \
+    -lm $dir/gale.o3g.${smoothing}_utf8.gz \
+    -ppl $dir/heldout_utf8.txt
+  ngram \
+    -unk \
+    -lm $dir/gale.o3g.${smoothing}_utf8.gz \
+    -ppl $dir/heldout_utf8.txt \
+    -debug 2 >& $dir/3gram.${smoothing}_utf8.ppl2
+  # 4gram language model
+  echo "training 4-gram lm"
+  ngram-count \
+    -text $dir/train_utf8.txt.gz \
+    -order 4 \
+    -limit-vocab \
+    -vocab $dir/wordlist.txt \
+    -unk -map-unk "<UNK>" \
+    -${smoothing}discount -interpolate \
+    -lm $dir/gale.o4g.${smoothing}_utf8.gz
+  echo "PPL for GALE Arabic 4gram LM:"
+  ngram \
+    -unk \
+    -lm $dir/gale.o4g.${smoothing}_utf8.gz \
+    -ppl $dir/heldout_utf8.txt
+  ngram \
+    -unk \
+    -lm $dir/gale.o4g.${smoothing}_utf8.gz \
+    -ppl $dir/heldout_utf8.txt \
+    -debug 2 >& $dir/4gram.${smoothing}_utf8.ppl2
+fi
---
+++ egs/tunisian_msa/s5o/run.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash 
+
+# Tunisian MSA  and 1 Libyan accented speaker
+# LM is trained on GALE training data and ARL LM training data.
+# Uses large Lexicon
+
+. ./cmd.sh
+. ./path.sh
+stage=0
+
+. ./utils/parse_options.sh
+
+set -e
+set -o pipefail
+set u
+
+tmpdir=data/local/tmp
+lexicon=/mnt/corpora/Tunisian_MSA/lexicon.txt
+
+if [ $stage -le 0 ]; then
+  local/prepare_data.sh || exit 1;
+fi
+
+if [ $stage -le 1 ]; then
+  mkdir -p $tmpdir/dict
+  local/prepare_dict.sh $lexicon
+fi
+
+if [ $stage -le 2 ]; then
+  # prepare the lang directory
+  utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
+fi
+
+if [ $stage -le 3 ]; then
+  # extract acoustic features
+  for fld in devtest train test; do
+    steps/make_mfcc.sh data/$fld exp/make_mfcc/$fld mfcc
+    utils/fix_data_dir.sh data/$fld
+    steps/compute_cmvn_stats.sh data/$fld exp/make_mfcc mfcc
+    utils/fix_data_dir.sh data/$fld
+  done
+fi
+
+if [ $stage -le 4 ]; then
+  echo "$0: monophone training"
+  steps/train_mono.sh  data/train data/lang exp/mono
+fi
+
+if [ $stage -le 5 ]; then
+  # align with monophones
+  steps/align_si.sh  data/train data/lang exp/mono exp/mono_ali
+fi
+
+if [ $stage -le 6 ]; then
+  echo "$0: Starting  triphone training in exp/tri1"
+  steps/train_deltas.sh \
+    --boost-silence 1.25 1000 6000 data/train data/lang exp/mono_ali exp/tri1
+fi
+
+if [ $stage -le 7 ]; then
+  # align with triphones
+  steps/align_si.sh  data/train data/lang exp/tri1 exp/tri1_ali
+fi
+
+if [ $stage -le 8 ]; then
+  echo "$0: Starting (lda_mllt) triphone training in exp/tri2b"
+  steps/train_lda_mllt.sh \
+    --splice-opts "--left-context=3 --right-context=3" 500 5000 \
+    data/train data/lang exp/tri1_ali exp/tri2b
+fi
+
+if [ $stage -le 9 ]; then
+  # align with lda and mllt adapted triphones
+  steps/align_si.sh \
+    --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali
+fi
+
+if [ $stage -le 10 ]; then
+  echo "$0: Starting (SAT) triphone training in exp/tri3b"
+  steps/train_sat.sh 800 8000 data/train data/lang exp/tri2b_ali exp/tri3b
+fi
+
+if [ $stage -le 11 ]; then
+  # align with tri3b models
+  echo "$0: Starting exp/tri3b_ali"
+  steps/align_fmllr.sh data/train data/lang exp/tri3b exp/tri3b_ali
+fi
+
+if [ $stage -le 12 ]; then
+  echo "$0: lm training."
+  rm -Rf data/local/lm
+  local/gale_arl_train_lms_utf8.sh \
+    ../../gale_arabic/s5d/data/train/text \
+    data/local/dict/lexicon.txt \
+    data/local/lm || exit 1; 
+fi
+
+if [ $stage -le 13 ]; then
+  echo "Making grammar fst."
+  local/format_lm_utf8.sh
+fi
+
+if [ $stage -le 14 ]; then
+  (
+    # make decoding graphs for SAT models
+    utils/mkgraph.sh \
+      data/lang_test \
+      exp/tri3b \
+      exp/tri3b/graph || exit 1;
+
+    # decode test sets with tri3b models
+    for x in devtest test; do
+      nspk=$(wc -l < data/$x/spk2utt)
+      steps/decode_fmllr.sh \
+        --nj $nspk \
+        exp/tri3b/graph \
+        data/$x \
+        exp/tri3b/decode_${x} || exit 1;
+    done
+  ) &
+fi
+
+if [ $stage -le 15 ]; then
+    # train and test chain models
+  local/chain/run_tdnn.sh
+fi
