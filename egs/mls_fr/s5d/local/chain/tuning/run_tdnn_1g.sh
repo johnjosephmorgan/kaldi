2b54a1a0738ad9d40d3a9dd4df23643b0b28bf45
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-07-07 17:28:36 -0400

    50k recipe.

---
+++ egs/mls_fr/s5d/local/chain/tuning/run_tdnn_1g.sh
@@ -0,0 +1,314 @@
+#!/usr/bin/env bash
+set -e
+
+# 1g is as 1f but trains on 100k utts
+# configs for 'chain'
+stage=0
+decode_nj=28
+train_set=train_100k
+gmm=tri3b
+nnet3_affix=
+
+# The rest are configs specific to this script.  Most of the parameters
+# are just hardcoded at this level, in the commands below.
+affix=1g
+tree_affix=
+train_stage=-10
+get_egs_stage=-10
+decode_iter=
+
+# TDNN options
+frames_per_eg=150,110,100
+remove_egs=true
+common_egs_dir=
+xent_regularize=0.1
+dropout_schedule='0,0@0.20,0.5@0.50,0'
+
+test_online_decoding=true  # if true, it will run the last decoding stage.
+
+# End configuration section.
+echo "$0 $@"  # Print the command line for logging
+
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
+
+if ! cuda-compiled; then
+  cat <<EOF && exit 1
+This script is intended to be used with GPUs but you have not compiled Kaldi with CUDA
+If you want to use GPUs (and have them), go to src/, and configure and make on a machine
+where "nvcc" is installed.
+EOF
+fi
+
+# The iVector-extraction and feature-dumping parts are the same as the standard
+# nnet3 setup, and you can skip them by setting "--stage 11" if you have already
+# run those things.
+
+local/nnet3/run_ivector_common.sh --stage $stage \
+                                  --train-set $train_set \
+                                  --gmm $gmm \
+                                  --num-threads-ubm 6 --num-processes 3 \
+                                  --nnet3-affix "$nnet3_affix" || exit 1;
+
+gmm_dir=exp/$gmm
+ali_dir=exp/${gmm}_ali_${train_set}_sp
+tree_dir=exp/chain${nnet3_affix}/tree_sp${tree_affix:+_$tree_affix}
+lang=data/lang_chain
+lat_dir=exp/chain${nnet3_affix}/${gmm}_${train_set}_sp_lats
+dir=exp/chain/tdnn${affix:+_$affix}_sp
+train_data_dir=data/${train_set}_sp_hires
+lores_train_data_dir=data/${train_set}_sp
+train_ivector_dir=exp/nnet3${nnet3_affix}/ivectors_${train_set}_sp_hires
+
+# if we are using the speed-perturbed data we need to generate
+# alignments for it.
+
+for f in $gmm_dir/final.mdl $train_data_dir/feats.scp $train_ivector_dir/ivector_online.scp \
+    $lores_train_data_dir/feats.scp $ali_dir/ali.1.gz; do
+  [ ! -f $f ] && echo "$0: expected file $f to exist" && exit 1
+done
+
+# Please take this as a reference on how to specify all the options of
+# local/chain/run_chain_common.sh
+if [ $stage -le 11 ]; then
+    local/chain/run_chain_common.sh \
+    --stage $stage \
+    --gmm-dir $gmm_dir \
+    --ali-dir $ali_dir \
+    --lores-train-data-dir ${lores_train_data_dir} \
+    --lang $lang \
+    --lat-dir $lat_dir \
+    --num-leaves 7000 \
+    --tree-dir $tree_dir || exit 1;
+fi
+
+if [ $stage -le 14 ]; then
+  echo "$0: creating neural net configs using the xconfig parser";
+
+  num_targets=$(tree-info $tree_dir/tree | grep num-pdfs | awk '{print $2}')
+  learning_rate_factor=$(echo "print (0.5/$xent_regularize)" | python)
+  affine_opts="l2-regularize=0.008 dropout-proportion=0.0 dropout-per-dim=true dropout-per-dim-continuous=true"
+  tdnnf_opts="l2-regularize=0.008 dropout-proportion=0.0 bypass-scale=0.75"
+  linear_opts="l2-regularize=0.008 orthonormal-constraint=-1.0"
+  prefinal_opts="l2-regularize=0.008"
+  output_opts="l2-regularize=0.002"
+
+  mkdir -p $dir/configs
+
+  cat <<EOF > $dir/configs/network.xconfig
+  input dim=100 name=ivector
+  input dim=40 name=input
+
+  # please note that it is important to have input layer with the name=input
+  # as the layer immediately preceding the fixed-affine-layer to enable
+  # the use of short notation for the descriptor
+  fixed-affine-layer name=lda input=Append(-1,0,1,ReplaceIndex(ivector, t, 0)) affine-transform-file=$dir/configs/lda.mat
+
+  # the first splicing is moved before the lda layer, so no splicing here
+  relu-batchnorm-dropout-layer name=tdnn1 $affine_opts dim=1536
+  tdnnf-layer name=tdnnf2 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=1
+  tdnnf-layer name=tdnnf3 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=1
+  tdnnf-layer name=tdnnf4 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=1
+  tdnnf-layer name=tdnnf5 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=0
+  tdnnf-layer name=tdnnf6 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf7 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf8 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf9 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf10 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf11 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf12 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf13 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf14 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf15 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf16 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  tdnnf-layer name=tdnnf17 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+  linear-component name=prefinal-l dim=256 $linear_opts
+
+  prefinal-layer name=prefinal-chain input=prefinal-l $prefinal_opts big-dim=1536 small-dim=256
+  output-layer name=output include-log-softmax=false dim=$num_targets $output_opts
+
+  prefinal-layer name=prefinal-xent input=prefinal-l $prefinal_opts big-dim=1536 small-dim=256
+  output-layer name=output-xent dim=$num_targets learning-rate-factor=$learning_rate_factor $output_opts
+EOF
+  steps/nnet3/xconfig_to_configs.py --xconfig-file $dir/configs/network.xconfig --config-dir $dir/configs/
+fi
+
+if [ $stage -le 15 ]; then
+  steps/nnet3/chain/train.py --stage $train_stage \
+    --cmd "$decode_cmd" \
+    --feat.online-ivector-dir $train_ivector_dir \
+    --feat.cmvn-opts "--norm-means=false --norm-vars=false" \
+    --chain.xent-regularize $xent_regularize \
+    --chain.leaky-hmm-coefficient 0.1 \
+    --chain.l2-regularize 0.0 \
+    --chain.apply-deriv-weights false \
+    --chain.lm-opts="--num-extra-lm-states=2000" \
+    --egs.dir "$common_egs_dir" \
+    --egs.stage $get_egs_stage \
+    --egs.opts "--frames-overlap-per-eg 0 --constrained false" \
+    --egs.chunk-width $frames_per_eg \
+    --trainer.dropout-schedule $dropout_schedule \
+    --trainer.add-option="--optimization.memory-compression-level=2" \
+    --trainer.num-chunk-per-minibatch 64 \
+    --trainer.frames-per-iter 2500000 \
+    --trainer.num-epochs 4 \
+    --trainer.optimization.num-jobs-initial 2 \
+    --trainer.optimization.num-jobs-final 2 \
+    --trainer.optimization.initial-effective-lrate 0.00015 \
+    --trainer.optimization.final-effective-lrate 0.000015 \
+    --trainer.max-param-change 2.0 \
+    --cleanup.remove-egs $remove_egs \
+    --feat-dir $train_data_dir \
+    --tree-dir $tree_dir \
+    --lat-dir $lat_dir \
+    --dir $dir  || exit 1;
+
+fi
+
+graph_dir=$dir/graph
+if [ $stage -le 16 ]; then
+  # Note: it might appear that this $lang directory is mismatched, and it is as
+  # far as the 'topo' is concerned, but this script doesn't read the 'topo' from
+  # the lang directory.
+  utils/mkgraph.sh \
+    --self-loop-scale 1.0 \
+    --remove-oov \
+    data/lang_test \
+    $dir \
+    $graph_dir
+fi
+
+iter_opts=
+if [ ! -z $decode_iter ]; then
+  iter_opts=" --iter $decode_iter "
+fi
+
+if [ $stage -le 17 ]; then
+  rm $dir/.error 2>/dev/null || true
+  # note: if the features change (e.g. you add pitch features), you will have to
+  # change the options of the following command line.
+  steps/online/nnet3/prepare_online_decoding.sh \
+    --mfcc-config conf/mfcc_hires.conf \
+    $lang \
+    exp/nnet3${nnet3_affix}/extractor \
+    $dir \
+    ${dir}_online
+
+  rm $dir/.error 2>/dev/null || true
+  for data in test; do
+    (
+      nspk=$(wc -l <data/${data}_hires/spk2utt)
+      # note: we just give it "data/${data}" as it only uses the wav.scp, the
+      # feature type does not matter.
+      steps/online/nnet3/decode.sh \
+	  --cmd "$decode_cmd" \
+	  --post-decode-acwt 10.0 \
+          --acwt 1.0 \
+          --nj $nspk \
+          $graph_dir \
+	  data/${data} \
+	  ${dir}_online/decode_${data} || exit 1
+    ) || touch $dir/.error &
+  done
+  wait
+  if [ -f $dir/.error ]; then
+    echo "$0: something went wrong in decoding"
+    exit 1
+  fi
+fi
+
+if [ $stage -le 19 ]; then
+  [ ! -d data/yaounde ] || rm -Rf data/yaounde
+  mkdir -p data/yaounde
+  (
+    cd data/yaounde
+    ln -s ../../../../yaounde/s5/data/ca16 ./
+    ln -s ../../../../yaounde/s5/data/lang_test ./
+  )
+fi
+
+if [ $stage -le 20 ]; then
+  utils/mkgraph.sh \
+    --self-loop-scale 1.0 \
+    --remove-oov \
+    data/yaounde/lang_test \
+    $dir \
+    ${graph_dir}_yaounde
+fi
+
+if [ $stage -le 21 ]; then
+  (
+    # note: we just give it "data/${data}" as it only uses the wav.scp, the
+    # feature type does not matter.
+    steps/online/nnet3/decode.sh \
+      --acwt 1.0 \
+      --cmd "$decode_cmd" \
+      --nj 8 \
+      --post-decode-acwt 10.0 \
+      ${graph_dir}_yaounde \
+      data/yaounde/ca16 \
+      ${dir}_online/decode_yaounde_ca16 || exit 1
+  ) || touch $dir/.error &
+  wait
+  if [ -f $dir/.error ]; then
+    echo "$0: something went wrong in decoding"
+    exit 1
+  fi
+fi
+
+if [ $stage -le 22 ]; then
+  mkdir -p data/local/tmp/mls_fr/lm
+  cut -f 2- data/dev/text > data/local/tmp/mls_fr/lm/corpus
+  cut -f 2- data/train/text > data/local/tmp/mls_fr/lm/corpus
+  local/prepare_lm.sh  \
+    data/local/tmp/mls_fr/lm/corpus || exit 1;
+fi
+
+if [ $stage -le 23 ]; then
+  local/format_lms.sh \
+    --src-dir data/lang \
+    data/local/lm
+fi
+
+if [ $stage -le 24 ]; then
+  utils/mkgraph.sh \
+    --self-loop-scale 1.0 \
+    --remove-oov \
+    data/lang_test \
+    $dir \
+    ${graph_dir}_lm_from_mls_fr_training_text
+fi
+
+if [ $stage -le 25 ]; then
+  for f in test; do
+    (
+      steps/online/nnet3/decode.sh \
+          --acwt 1.0 \
+          --cmd "$decode_cmd" \
+	  --nj 8 \
+	  --post-decode-acwt 10.0 \
+	  ${graph_dir}_lm_from_mls_fr_training_text \
+          data/$f \
+          ${dir}_online/decode_mls_${f} || exit 1
+    ) || touch $dir/.error &
+    wait
+    if [ -f $dir/.error ]; then
+      echo "$0: something went wrong in decoding"
+      exit 1
+    fi
+  done
+fi
+
+if [ $stage -le 26 ]; then
+  steps/online/nnet3/decode.sh \
+    --acwt 1.0 \
+    --cmd "$decode_cmd" \
+    --nj 8 \
+    --post-decode-acwt 10.0 \
+    ${graph_dir}_lm_from_mls_fr_training_text \
+    data/yaounde/ca16 \
+    ${dir}_online/decode_mls_lm_yaounde_ca16 || exit 1
+fi
+
+exit 0;39
---
+++ egs/mls_fr/s5d/run.sh
@@ -0,0 +1,252 @@
+#!/usr/bin/env bash
+
+# Multilingual Libri speech French
+# Train on part of the train set
+# 50k utterances
+data=/mnt/corpora/MLS_French
+mfccdir=mfcc
+tmp_dir=data/local/tmp
+g2p_input_text_files="data/dev/text data/train/text"
+
+stage=0
+
+. ./cmd.sh
+. ./path.sh
+. parse_options.sh
+
+# you might not want to do this for interactive shells.
+set -e
+
+if [ $stage -le 0 ]; then
+  echo "$0: Data Preparation."
+  for f in dev test train; do
+    local/data_prep.sh $data/$f data/$f
+  done
+fi
+
+if [ $stage -le 1 ]; then
+  echo "$0: Prepare lexicon as in yaounde recipe."
+  mkdir -p data/local/tmp/dict
+  export LC_ALL=C
+  local/prepare_dict.sh \
+    local/dict/santiago.txt \
+    $tmp_dir/dict/init || exit 1;
+fi
+
+if [ $stage -le 2 ]; then
+  echo "$0: Training a g2p model."
+  local/g2p/train_g2p.sh \
+    $tmp_dir/dict/init \
+    $tmp_dir/dict/g2p || exit 1;
+fi
+
+if [ $stage -le 3 ]; then
+  echo "$0: Applying the g2p."
+  local/g2p/apply_g2p.sh \
+    $tmp_dir/dict/g2p/model.fst \
+    $tmp_dir/dict/work \
+    $tmp_dir/dict/init/lexicon.txt \
+    $tmp_dir/dict/init/lexicon_with_tabs.txt \
+    $g2p_input_text_files || exit 1;
+fi
+
+if [ $stage -le 4    ]; then
+  echo "$0: Delimiting fields with space instead of tabs."
+  mkdir -p $tmp_dir/dict/final
+  expand -t 1 \
+    $tmp_dir/dict/init/lexicon_with_tabs.txt > \
+    $tmp_dir/dict/final/lexicon.txt
+fi
+
+if [ $stage -le 5    ]; then
+  echo "$0: Preparing expanded lexicon."
+  local/prepare_dict.sh \
+    $tmp_dir/dict/final/lexicon.txt \
+    data/local/dict || exit 1;
+  echo "$0: Adding <UNK> to the lexicon."
+  echo "<UNK> SPN" >> data/local/dict/lexicon.txt
+fi
+
+if [ $stage -le 6 ]; then
+  echo "$0: Preparing lang directory."
+  utils/prepare_lang.sh \
+    --position-dependent-phones true \
+    data/local/dict \
+    "<UNK>" \
+    data/local/lang_tmp \
+    data/lang || exit 1;
+fi
+
+if [ $stage -le 7 ]; then
+  echo "$0: Preparing the lm."
+  mkdir -p $tmp_dir/lm
+  mkdir -p data/local/lm
+  cut -f 2- data/dev/text > $tmp_dir/lm/text
+  cut -f 2- data/train/text >> $tmp_dir/lm/text
+  local/prepare_lm.sh  $tmp_dir/lm/text || exit 1;
+fi
+
+if [ $stage -le 8 ]; then
+  echo "$0: Make the G.fst."
+  local/format_lms.sh \
+    --src-dir data/lang \
+    data/local/lm
+fi
+
+if [ $stage -le 9 ]; then
+  for part in dev test train; do
+    echo "$0: Extracting features for $part."
+    steps/make_mfcc.sh --cmd "$train_cmd" --nj 40 data/$part exp/make_mfcc/$part $mfccdir
+    steps/compute_cmvn_stats.sh data/$part exp/make_mfcc/$part $mfccdir
+  done
+fi
+
+if [ $stage -le 10 ]; then
+  echo "$0: Make some small data subsets for early system-build stages."
+  # For the monophone stages we select the shortest utterances, which should make it
+  # easier to align the data from a flat start.
+  utils/subset_data_dir.sh --shortest data/train 2000 data/train_2kshort
+  utils/subset_data_dir.sh data/train 5000 data/train_5k
+  utils/subset_data_dir.sh data/train 25000 data/train_25k
+  utils/subset_data_dir.sh data/train 50000 data/train_50k
+fi
+
+if [ $stage -le 11 ]; then
+  steps/train_mono.sh \
+    --boost-silence 1.25 \
+    --nj 20 \
+    --cmd "$train_cmd" \
+    data/train_2kshort \
+    data/lang \
+    exp/mono
+fi
+
+if [ $stage -le 12 ]; then
+  steps/align_si.sh \
+    --boost-silence 1.25 \
+    --cmd "$train_cmd" \
+    --nj 10 \
+    data/train_5k \
+    data/lang \
+    exp/mono \
+    exp/mono_ali_5k
+
+  # train a first delta + delta-delta triphone system on a subset of 1000 utterances
+  steps/train_deltas.sh \
+    --boost-silence 1.25 \
+    --cmd "$train_cmd" \
+    1000 5000 \
+    data/train_5k \
+    data/lang \
+    exp/mono_ali_5k \
+    exp/tri1
+fi
+
+if [ $stage -le 13 ]; then
+  steps/align_si.sh \
+    --cmd "$train_cmd" \
+    --nj 10 \
+    data/train_25k \
+    data/lang \
+    exp/tri1 \
+    exp/tri1_ali_25k
+
+  # train an LDA+MLLT system.
+  steps/train_lda_mllt.sh \
+    --cmd "$train_cmd" \
+    --splice-opts "--left-context=3 --right-context=3" \
+    1500 10000 \
+    data/train_25k \
+    data/lang \
+    exp/tri1_ali_25k \
+    exp/tri2b
+fi
+
+if [ $stage -le 14 ]; then
+  # Align a 50k utts subset using the tri2b model
+  steps/align_si.sh  \
+    --cmd "$train_cmd" \
+    --nj 10 \
+    --use-graphs true \
+    data/train_50k \
+    data/lang \
+    exp/tri2b \
+    exp/tri2b_ali_50k || exit 1;
+
+  # Train tri3b, which is LDA+MLLT+SAT 
+  steps/train_sat.sh \
+    --cmd "$train_cmd" \
+    2500 15000 \
+    data/train_50k \
+    data/lang \
+    exp/tri2b_ali_50k \
+    exp/tri3b || exit 1;
+fi
+
+if [ $stage -le 15 ]; then
+  # Now we compute the pronunciation and silence probabilities from training data,
+  # and re-create the lang directory.
+  steps/get_prons.sh \
+    --cmd "$train_cmd" \
+    data/train_100k \
+    data/lang \
+    exp/tri3b
+fi
+
+if [ $stage -le 16 ]; then
+  utils/dict_dir_add_pronprobs.sh \
+    --max-normalize true \
+    data/local/dict \
+    exp/tri3b/pron_counts_nowb.txt \
+    exp/tri3b/sil_counts_nowb.txt \
+    exp/tri3b/pron_bigram_counts_nowb.txt \
+    data/local/dict_with_new_prons
+fi
+
+if [ $stage -le 17 ]; then
+  utils/prepare_lang.sh \
+    data/local/dict_with_new_prons \
+    "<UNK>" \
+    data/local/lang_tmp \
+    data/lang
+fi
+
+if [ $stage -le 18 ]; then
+  local/format_lms.sh \
+    --src-dir data/lang \
+    data/local/lm
+fi
+
+if [ $stage -le  19 ]; then
+  steps/align_fmllr.sh \
+    --cmd "$train_cmd" \
+    --nj 34 \
+    data/train_50k \
+    data/lang \
+    exp/tri3b \
+    exp/tri3b_ali || exit 1;
+fi
+
+if [ $stage -le 20 ]; then
+  # train and test nnet3 tdnn models 
+  local/chain/tuning/run_tdnn_1h.sh
+fi
+
+if [ $stage -le 21 ]; then
+  # decode using the tri3b model
+  utils/mkgraph.sh \
+    data/lang_test \
+    exp/tri3b \
+    exp/tri3b/graph
+fi
+
+if [ $stage -le 22 ]; then
+  for test in test dev; do
+  steps/decode_fmllr.sh \
+    --cmd "$decode_cmd" \
+    --nj 20 \
+    exp/tri3b/graph \
+    data/$test \
+    exp/tri3b/decode_$test
+  done
+fi
